8c5f13e60efd74a5bc267fc182ac5ca892cbf50a
Chia-Ping Tsai <chia7712@gmail.com>
2021-09-03 12:20:35 +0800

    apply google java format

--- app/src/test/java/org/astraea/AppTest.java
+++ app/src/test/java/org/astraea/AppTest.java
@@ -3,12 +3,14 @@
  */
 package org.astraea;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+  @Test
+  void appHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+  }
 }
