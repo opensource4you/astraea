9851f32d4f4252d6fe7602ce374d7b2d0576a3ac
Chia-Ping Tsai <chia7712@gmail.com>
2021-09-03 12:28:37 +0800

    apply google java format (#4)

--- app/src/main/java/org/astraea/App.java
+++ app/src/main/java/org/astraea/App.java
@@ -4,11 +4,11 @@
 package org.astraea;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+  public String getGreeting() {
+    return "Hello World!";
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getGreeting());
+  }
 }
