cd7be7807bc257806dec5f9932157c20e5eef7af
Zheng-Xian Li <garyparrottt@gmail.com>
2021-09-09 08:24:53 +0800

    implement test for JmxBrokerMetricsClient & Metrics

---
+++ app/src/test/java/org/astraea/metrics/jmx/JmxBrokerMetricsClientTest.java
@@ -0,0 +1,29 @@
+package org.astraea.metrics.jmx;
+
+import java.net.MalformedURLException;
+import javax.management.remote.JMXServiceURL;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class JmxBrokerMetricsClientTest {
+
+  @Test
+  void testFetchMetricWithoutConnectWillThrowError() throws MalformedURLException {
+    final String JMX_URI_FORMAT = "service:jmx:rmi:///jndi/rmi://" + "%s" + ":" + "%d" + "/jmxrmi";
+    final String url = String.format(JMX_URI_FORMAT, "example.com", 5566);
+    JMXServiceURL jmxServiceURL = new JMXServiceURL(url);
+
+    JmxBrokerMetricsClient client = new JmxBrokerMetricsClient(jmxServiceURL);
+
+    Assertions.assertThrows(
+        IllegalStateException.class, () -> client.fetchJmxMetric(new JmxBrokerMetric("", "")));
+    Assertions.assertThrows(
+        IllegalStateException.class, () -> client.fetchMetric(new DoubleBrokerMetric("", "")));
+    Assertions.assertThrows(
+        IllegalStateException.class, () -> client.fetchMetric(new IntegerBrokerMetric("", "")));
+    Assertions.assertThrows(
+        IllegalStateException.class, () -> client.fetchMetric(new LongBrokerMetric("", "")));
+    Assertions.assertThrows(
+        IllegalStateException.class, () -> client.fetchMetric(new StringBrokerMetric("", "")));
+  }
+}
---
+++ app/src/test/java/org/astraea/metrics/jmx/MetricsTest.java
@@ -0,0 +1,23 @@
+package org.astraea.metrics.jmx;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class MetricsTest {
+
+  @Nested
+  @DisplayName("Ensure all predefined metrics won't throw error during initialization")
+  class EnsureAllPredefinedMetricsWillNotThrowErrorDuringInitialization {
+
+    @Test
+    void brokerTopic() {
+      Assertions.assertDoesNotThrow(() -> Metrics.BrokerTopic.MessageInPerSecond.count);
+      Assertions.assertDoesNotThrow(() -> Metrics.BrokerTopic.MessageInPerSecond.fifteenMinuteRate);
+      Assertions.assertDoesNotThrow(() -> Metrics.BrokerTopic.MessageInPerSecond.fiveMinuteRate);
+      Assertions.assertDoesNotThrow(() -> Metrics.BrokerTopic.MessageInPerSecond.meanRate);
+      Assertions.assertDoesNotThrow(() -> Metrics.BrokerTopic.MessageInPerSecond.oneMinuteRate);
+    }
+  }
+}
