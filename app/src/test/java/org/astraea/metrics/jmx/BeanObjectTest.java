53c65f32afc5e88e6f2dd8e14e220ad6a3f499b8
Zheng-Xian Li <garyparrottt@gmail.com>
2021-09-17 01:47:22 +0800

    remove worthless test

--- app/src/test/java/org/astraea/metrics/jmx/BeanObjectTest.java
+++ app/src/test/java/org/astraea/metrics/jmx/BeanObjectTest.java
@@ -1,26 +1,3 @@
 package org.astraea.metrics.jmx;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.HashMap;
-import java.util.Map;
-import org.junit.jupiter.api.Test;
-
-class BeanObjectTest {
-
-  @Test
-  void constructorShouldMakeDefensiveCopy() {
-    Map<String, String> properties = new HashMap<>();
-    Map<String, Object> attributes = new HashMap<>();
-
-    // construct the bean object with two given map
-    BeanObject beanObject = new BeanObject("example.com", properties, attributes);
-
-    // modify the maps
-    properties.put("key", "value");
-    attributes.put("key", "value");
-    // if the constructor have make defensive copy, the original given map won't affect their value
-    assertTrue(beanObject.getPropertyView().isEmpty());
-    assertTrue(beanObject.getAttributeView().isEmpty());
-  }
-}
+class BeanObjectTest {}
