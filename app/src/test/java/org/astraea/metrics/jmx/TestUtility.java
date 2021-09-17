78673a4f800a02803446d4b1f58b46ccc25cc8c9
Zheng-Xian Li <garyparrottt@gmail.com>
2021-09-17 08:59:17 +0800

    revise BeanObjectTest.java

---
+++ app/src/test/java/org/astraea/metrics/jmx/TestUtility.java
@@ -0,0 +1,24 @@
+package org.astraea.metrics.jmx;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+public class TestUtility {
+
+  static String getDomainName(BeanObject object) throws MalformedObjectNameException {
+    return ObjectName.getInstance(object.objectName()).getDomain();
+  }
+
+  static HashMap<String, String> getPropertyList(BeanObject object)
+      throws MalformedObjectNameException {
+    Hashtable<String, String> keyPropertyList =
+        ObjectName.getInstance(object.objectName()).getKeyPropertyList();
+    HashMap<String, String> hm = new HashMap<>();
+    for (String s : keyPropertyList.keySet()) {
+      hm.put(s, keyPropertyList.get(s));
+    }
+    return hm;
+  }
+}
