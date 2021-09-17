02c486216e991e0a5da27c31035b8a62328b2b5b
Zheng-Xian Li <garyparrottt@gmail.com>
2021-09-17 09:32:01 +0800

    test for BeanQuery

--- app/src/test/java/org/astraea/metrics/jmx/TestUtility.java
+++ app/src/test/java/org/astraea/metrics/jmx/TestUtility.java
@@ -11,10 +11,25 @@ public class TestUtility {
     return ObjectName.getInstance(object.objectName()).getDomain();
   }
 
+  static String getDomainName(BeanQuery object) throws MalformedObjectNameException {
+    return ObjectName.getInstance(object.queryString()).getDomain();
+  }
+
   static HashMap<String, String> getPropertyList(BeanObject object)
       throws MalformedObjectNameException {
     Hashtable<String, String> keyPropertyList =
         ObjectName.getInstance(object.objectName()).getKeyPropertyList();
+    HashMap<String, String> hm = new HashMap<>();
+    for (String s : keyPropertyList.keySet()) {
+      hm.put(s, keyPropertyList.get(s));
+    }
+    return hm;
+  }
+
+  static HashMap<String, String> getPropertyList(BeanQuery object)
+      throws MalformedObjectNameException {
+    Hashtable<String, String> keyPropertyList =
+        ObjectName.getInstance(object.queryString()).getKeyPropertyList();
     HashMap<String, String> hm = new HashMap<>();
     for (String s : keyPropertyList.keySet()) {
       hm.put(s, keyPropertyList.get(s));
