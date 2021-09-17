376586781364748162121c8de8f09005c0fb04ec
Zheng-Xian Li <garyparrottt@gmail.com>
2021-09-17 22:00:40 +0800

    test for MBeanClientTest.java

--- app/src/test/java/org/astraea/metrics/jmx/MBeanClientTest.java
+++ app/src/test/java/org/astraea/metrics/jmx/MBeanClientTest.java
@@ -115,4 +115,98 @@ class MBeanClientTest {
       assertEquals(answer, beanObject);
     }
   }
+
+  @Test
+  void testMBeanClientFetch() throws Exception {
+    // arrange
+    final ObjectName fetchMe = ObjectName.getInstance("org.example", "type", "object1");
+    final ObjectName objectName2 = ObjectName.getInstance("org.example", "type", "object2");
+    final ObjectName objectName3 = ObjectName.getInstance("org.astraea", "type", "object3");
+    final ObjectName objectName4 = ObjectName.getInstance("org.astraea", "type", "object4");
+
+    final Map<ObjectName, Object> mbeansServerContent =
+        Map.of(
+            fetchMe, new TestUtility.OneAttribute<String>("FetchMe"),
+            objectName2, new TestUtility.OneAttribute<String>("value2"),
+            objectName3, new TestUtility.TwoAttribute<String, Integer>("value3", 3),
+            objectName4, new TestUtility.TwoAttribute<String, Integer>("value4", 4));
+
+    JMXConnectorServer jmxConnectorServer = TestUtility.setupJmxServerSpy(mbeansServerContent);
+    jmxConnectorServer.start();
+    MBeanClient client = new MBeanClient(jmxConnectorServer.getAddress());
+    client.connect();
+
+    // act
+    BeanObject fetchResult =
+        client.fetchObjectAttribute(
+            BeanObject.fromDomainName("org.example")
+                .selectProperty("type", "object1")
+                .fetchAttribute("Value"));
+
+    // assert
+    assertEquals(fetchMe.getKeyPropertyList(), fetchResult.getPropertyView());
+    assertEquals("FetchMe", fetchResult.getAttributeView().get("Value"));
+
+    client.close();
+  }
+
+  @Test
+  void testMBeanClientQueryAndFetch() throws Exception {
+    // arrange 1
+    final ObjectName target1 = ObjectName.getInstance("org.example", "type", "object1");
+    final ObjectName target2 = ObjectName.getInstance("org.example", "type", "object2");
+    final ObjectName target3 = ObjectName.getInstance("org.astraea", "type", "object3");
+    final ObjectName target4 = ObjectName.getInstance("org.astraea", "type", "object4");
+
+    final Map<ObjectName, Object> mbeansServerContent =
+        Map.of(
+            target1, new TestUtility.OneAttribute<String>("value1"),
+            target2, new TestUtility.OneAttribute<String>("value2"),
+            target3, new TestUtility.TwoAttribute<String, Integer>("value3", 3),
+            target4, new TestUtility.TwoAttribute<String, Integer>("value4", 4));
+
+    JMXConnectorServer jmxConnectorServer = TestUtility.setupJmxServerSpy(mbeansServerContent);
+    jmxConnectorServer.start();
+    MBeanClient client = new MBeanClient(jmxConnectorServer.getAddress());
+    client.connect();
+
+    final BeanObject expectResult1 =
+        BeanObject.fromDomainName("org.example").selectProperty("type", "object1");
+    final BeanObject expectResult2 =
+        BeanObject.fromDomainName("org.example").selectProperty("type", "object2");
+    final BeanObject expectResult3 =
+        BeanObject.fromDomainName("org.astraea").selectProperty("type", "object3");
+    final BeanObject expectResult4 =
+        BeanObject.fromDomainName("org.astraea").selectProperty("type", "object4");
+
+    // act 1
+    Set<BeanObject> queryResult1 =
+        client.queryObject(BeanQuery.forDomainName("org.*").whereProperty("type", "object?"));
+
+    // assert 1
+    assertEquals(4, queryResult1.size());
+    assertTrue(queryResult1.contains(expectResult1));
+    assertTrue(queryResult1.contains(expectResult2));
+    assertTrue(queryResult1.contains(expectResult3));
+    assertTrue(queryResult1.contains(expectResult4));
+
+    // arrange 2 Query & Fetch Attributes
+    BeanObject expectedAnswer =
+        new BeanObject("org.example", Map.of("type", "object1"), Map.of("Value", "value1"));
+
+    // act 2 query
+    Set<BeanObject> query =
+        client.queryObject(BeanQuery.forDomainName("org.example").whereProperty("type", "object1"));
+    BeanObject queryResult2 = query.stream().findFirst().get();
+    // act 2 update fetch target's attribute
+    BeanObject fetchTarget = queryResult2.fetchAttribute("Value");
+    // act 2 fetch object
+    BeanObject fetchResult = client.fetchObjectAttribute(fetchTarget);
+
+    // assert 2
+    assertEquals(1, query.size());
+    assertEquals(expectedAnswer, fetchResult);
+
+    client.close();
+  }
 }
