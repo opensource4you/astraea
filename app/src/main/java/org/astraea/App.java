24e8ff0ee9ccb5fd04b241bae2ac72cda401ae28
Chia-Ping Tsai <chia7712@gmail.com>
2021-09-26 23:38:14 +0800

    Offset explorer - used to expose the earliest offset and latest offset for topics (#29)

--- app/src/main/java/org/astraea/App.java
+++ app/src/main/java/org/astraea/App.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import org.astraea.offset.OffsetExplorer;
 import org.astraea.performance.latency.End2EndLatency;
 
 public class App {
-  private static final List<Class<?>> MAIN_CLASSES = Arrays.asList(End2EndLatency.class);
+  private static final List<Class<?>> MAIN_CLASSES =
+      Arrays.asList(End2EndLatency.class, OffsetExplorer.class);
 
   private static String toString(List<Class<?>> mains) {
     return mains.stream().map(Class::getName).collect(Collectors.joining(","));
