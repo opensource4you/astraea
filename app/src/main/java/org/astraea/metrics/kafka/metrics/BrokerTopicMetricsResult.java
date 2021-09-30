4753e007bc77fbd314930d7cd8b260447d2126b0
Zheng-Xian Li <garyparrottt@gmail.com>
2021-10-01 00:19:04 +0800

    spotless

--- app/src/main/java/org/astraea/metrics/kafka/metrics/BrokerTopicMetricsResult.java
+++ app/src/main/java/org/astraea/metrics/kafka/metrics/BrokerTopicMetricsResult.java
@@ -1,12 +1,11 @@
 package org.astraea.metrics.kafka.metrics;
 
+import java.util.Map;
 import org.astraea.metrics.jmx.BeanObject;
 import org.astraea.metrics.kafka.metrics.modifiers.HasCount;
 import org.astraea.metrics.kafka.metrics.modifiers.HasEventType;
 import org.astraea.metrics.kafka.metrics.modifiers.HasRate;
 import org.astraea.metrics.kafka.metrics.modifiers.MetricsResult;
-
-import java.util.Map;
 
 public class BrokerTopicMetricsResult extends MetricsResult
     implements HasCount, HasEventType, HasRate {
@@ -23,10 +22,10 @@ public class BrokerTopicMetricsResult extends MetricsResult
     StringBuilder sb = new StringBuilder();
     for (Map.Entry<String, Object> e : beanObject().getAttributes().entrySet()) {
       sb.append(System.lineSeparator())
-              .append("  ")
-              .append(e.getKey())
-              .append("=")
-              .append(e.getValue());
+          .append("  ")
+          .append(e.getKey())
+          .append("=")
+          .append(e.getValue());
     }
     return metric.name() + "{" + sb + "}";
   }
