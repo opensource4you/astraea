f1ff7bad2c837d32f7438cf1a3201bcc614f13ad
Zheng-Xian Li <garyparrottt@gmail.com>
2021-10-01 23:40:41 +0800

    remove redundant non-null statement

--- app/src/main/java/org/astraea/metrics/kafka/metrics/modifiers/HasCount.java
+++ app/src/main/java/org/astraea/metrics/kafka/metrics/modifiers/HasCount.java
@@ -1,9 +1,7 @@
 package org.astraea.metrics.kafka.metrics.modifiers;
 
-import java.util.Objects;
-
 public interface HasCount extends HasBeanObject {
   default long count() {
-    return (long) Objects.requireNonNull(beanObject().getAttributes().getOrDefault("Count", 0));
+    return (long) beanObject().getAttributes().getOrDefault("Count", 0);
   }
 }
