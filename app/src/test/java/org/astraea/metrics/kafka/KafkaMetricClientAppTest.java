700b75e6818440b3fd7263b14536a25b70cac497
Zheng-Xian Li <garyparrottt@gmail.com>
2021-10-01 23:32:50 +0800

    spotless

--- app/src/test/java/org/astraea/metrics/kafka/KafkaMetricClientAppTest.java
+++ app/src/test/java/org/astraea/metrics/kafka/KafkaMetricClientAppTest.java
@@ -1,33 +1,30 @@
 package org.astraea.metrics.kafka;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.net.MalformedURLException;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class KafkaMetricClientAppTest {
 
-    private final PrintStream stderr = System.err;
+  private final PrintStream stderr = System.err;
 
-    @BeforeEach
-    void beforeEach() {
-        System.setErr(new PrintStream(OutputStream.nullOutputStream()));
-    }
+  @BeforeEach
+  void beforeEach() {
+    System.setErr(new PrintStream(OutputStream.nullOutputStream()));
+  }
 
-    @AfterEach
-    void afterEach() {
-        System.setErr(stderr);
-    }
+  @AfterEach
+  void afterEach() {
+    System.setErr(stderr);
+  }
 
-    @Test
-    void testBadArguments() {
-        assertThrows(IllegalArgumentException.class, () -> KafkaMetricClientApp.main(new String[0]));
-        assertThrows(IllegalArgumentException.class, () -> KafkaMetricClientApp.main(new String[1]));
-    }
-
+  @Test
+  void testBadArguments() {
+    assertThrows(IllegalArgumentException.class, () -> KafkaMetricClientApp.main(new String[0]));
+    assertThrows(IllegalArgumentException.class, () -> KafkaMetricClientApp.main(new String[1]));
+  }
 }
