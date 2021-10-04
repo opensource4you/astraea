b064eb03f41043b05d090d5153783c26b54f5c02
chinghongfang <fjh7777@gmail.com>
2021-10-04 17:01:11 +0800

    Spotless Apply

--- app/src/main/java/org/astraea/argument/ArgumentUtil.java
+++ app/src/main/java/org/astraea/argument/ArgumentUtil.java
@@ -21,6 +21,7 @@ public class ArgumentUtil {
 
   /**
    * Side effect: parse args into toolArgument
+   *
    * @param toolArgument An argument object that the user want.
    * @param args Command line arguments that are put into main function.
    */
