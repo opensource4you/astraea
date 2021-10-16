d06f349608c6a2cb4b0d2d57cee4480fea362931
Zheng-Xian Li <garyparrottt@gmail.com>
2021-10-16 21:15:55 +0800

    delete the deprecated method
    Suddenly realize the new implementation can replace the deprecated usage
    without generate compile error. So ... I can just remove it, right?
    
    Right?

--- app/src/main/java/org/astraea/metrics/kafka/KafkaMetricClient.java
+++ app/src/main/java/org/astraea/metrics/kafka/KafkaMetricClient.java
@@ -8,8 +8,6 @@ import javax.management.remote.JMXServiceURL;
 import org.astraea.metrics.jmx.BeanObject;
 import org.astraea.metrics.jmx.BeanQuery;
 import org.astraea.metrics.jmx.MBeanClient;
-import org.astraea.metrics.kafka.metrics.BrokerTopicMetrics;
-import org.astraea.metrics.kafka.metrics.BrokerTopicMetricsResult;
 import org.astraea.metrics.kafka.metrics.Metric;
 
 public class KafkaMetricClient implements AutoCloseable {
@@ -34,23 +32,6 @@ public class KafkaMetricClient implements AutoCloseable {
     }
 
     return metric.from(resolved);
-  }
-
-  /**
-   * Request BrokerTopicMetrics.
-   *
-   * <p>For the available metrics, checkout {@link BrokerTopicMetrics}.
-   *
-   * @deprecated As this method couple the fetch/transform operation in the {@link
-   *     KafkaMetricClient} design, replaced by {@link #requestMetric(Metric)}
-   * @param metric the metric to request
-   * @return a {@link BrokerTopicMetricsResult} indicate the result
-   */
-  @Deprecated
-  public BrokerTopicMetricsResult requestMetric(BrokerTopicMetrics metric) {
-    // Java determine overloaded method call in compile-time, must do the transformation here to
-    // ensure we are calling the right method implementation.
-    return this.requestMetric((Metric<BrokerTopicMetricsResult>) metric);
   }
 
   @Override
