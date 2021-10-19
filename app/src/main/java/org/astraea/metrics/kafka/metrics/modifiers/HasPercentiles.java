615dfbfee3215a848811209a038f1678f4f6ef88
Zheng-Xian Li <garyparrottt@gmail.com>
2021-10-19 13:56:59 +0800

    Implementation of KafkaMetricClient (revised & new metrics)  (#55)

---
+++ app/src/main/java/org/astraea/metrics/kafka/metrics/modifiers/HasPercentiles.java
@@ -0,0 +1,28 @@
+package org.astraea.metrics.kafka.metrics.modifiers;
+
+public interface HasPercentiles extends HasBeanObject {
+
+  default double percentile50() {
+    return (double) beanObject().getAttributes().get("50thPercentile");
+  }
+
+  default double percentile75() {
+    return (double) beanObject().getAttributes().get("75thPercentile");
+  }
+
+  default double percentile95() {
+    return (double) beanObject().getAttributes().get("95thPercentile");
+  }
+
+  default double percentile98() {
+    return (double) beanObject().getAttributes().get("98thPercentile");
+  }
+
+  default double percentile99() {
+    return (double) beanObject().getAttributes().get("99thPercentile");
+  }
+
+  default double percentile999() {
+    return (double) beanObject().getAttributes().get("999thPercentile");
+  }
+}
---
+++ app/src/test/java/org/astraea/metrics/kafka/KafkaMetricsTest.java
@@ -0,0 +1,247 @@
+package org.astraea.metrics.kafka;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import javax.management.*;
+import javax.management.remote.JMXConnectorServer;
+import javax.management.remote.JMXConnectorServerFactory;
+import javax.management.remote.JMXServiceURL;
+import org.astraea.metrics.jmx.MBeanClient;
+import org.astraea.metrics.jmx.Utility;
+import org.astraea.metrics.kafka.metrics.BrokerTopicMetricsResult;
+import org.astraea.metrics.kafka.metrics.TotalTimeMs;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+class KafkaMetricsTest {
+
+  private MBeanServer mBeanServer;
+  private JMXConnectorServer jmxServer;
+  private Map<ObjectName, Object> registeredBeans = new HashMap<>();
+  private MBeanClient mBeanClient;
+
+  private void register(ObjectName name, Object mBean) {
+    registeredBeans.put(name, mBean);
+    try {
+      mBeanServer.registerMBean(mBean, name);
+    } catch (InstanceAlreadyExistsException
+        | MBeanRegistrationException
+        | NotCompliantMBeanException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private void clearRegisteredMBeans() {
+    registeredBeans.forEach(
+        (name, mbeans) -> {
+          try {
+            mBeanServer.unregisterMBean(name);
+          } catch (InstanceNotFoundException | MBeanRegistrationException e) {
+            throw new RuntimeException(e);
+          }
+        });
+    registeredBeans.clear();
+  }
+
+  @BeforeEach
+  void setUp() throws IOException {
+    JMXServiceURL serviceURL = new JMXServiceURL("service:jmx:rmi://127.0.0.1");
+
+    mBeanServer = ManagementFactory.getPlatformMBeanServer();
+
+    jmxServer = JMXConnectorServerFactory.newJMXConnectorServer(serviceURL, null, mBeanServer);
+    jmxServer.start();
+
+    mBeanClient = new MBeanClient(jmxServer.getAddress());
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+    jmxServer.stop();
+    clearRegisteredMBeans();
+    mBeanServer = null;
+    mBeanClient.close();
+  }
+
+  @Test
+  void testAllEnumNameUnique() {
+    // arrange act
+    Set<String> collectedName =
+        Arrays.stream(KafkaMetrics.BrokerTopic.values())
+            .map(KafkaMetrics.BrokerTopic::metricName)
+            .collect(Collectors.toSet());
+
+    // assert
+    assertEquals(KafkaMetrics.BrokerTopic.values().length, collectedName.size());
+  }
+
+  @ParameterizedTest
+  @EnumSource(value = KafkaMetrics.BrokerTopic.class)
+  void testRequestBrokerTopicMetrics(KafkaMetrics.BrokerTopic metric)
+      throws MalformedObjectNameException {
+    // arrange
+    Object mbean =
+        Utility.createReadOnlyDynamicMBean(
+            Map.of(
+                "Count", 1L,
+                "EventType", "bytes",
+                "FifteenMinuteRate", 2.0,
+                "FiveMinuteRate", 4.0,
+                "MeanRate", 8.0,
+                "OneMinuteRate", 16.0,
+                "RateUnit", TimeUnit.SECONDS));
+    register(
+        ObjectName.getInstance("kafka.server:type=BrokerTopicMetrics,name=" + metric.name()),
+        mbean);
+
+    // act
+    BrokerTopicMetricsResult result = metric.fetch(mBeanClient);
+
+    // assert
+    assertEquals(1L, result.count());
+    assertEquals("bytes", result.eventType());
+    assertEquals(2.0, result.fifteenMinuteRate());
+    assertEquals(4.0, result.fiveMinuteRate());
+    assertEquals(8.0, result.meanRate());
+    assertEquals(16.0, result.oneMinuteRate());
+    assertEquals(TimeUnit.SECONDS, result.rateUnit());
+  }
+
+  @ParameterizedTest()
+  @EnumSource(value = KafkaMetrics.Purgatory.class)
+  void testPurgatorySize(KafkaMetrics.Purgatory request) throws MalformedObjectNameException {
+    // arrange
+    Function<Integer, Object> fMbean =
+        (a) -> Utility.createReadOnlyDynamicMBean(Map.of("Value", a));
+    Function<String, String> fname =
+        (a) ->
+            String.format(
+                "kafka.server:type=DelayedOperationPurgatory,delayedOperation=%s,name=PurgatorySize",
+                a);
+    register(ObjectName.getInstance(fname.apply(request.name())), fMbean.apply(0xcafebabe));
+
+    // act
+    int size = request.size(mBeanClient);
+
+    // assert
+    assertEquals(0xcafebabe, size);
+  }
+
+  @ParameterizedTest()
+  @EnumSource(value = KafkaMetrics.Request.class)
+  void testRequestTotalTimeMs(KafkaMetrics.Request request) throws MalformedObjectNameException {
+    // arrange
+    Map<String, Object> map = new HashMap<>();
+    map.put("50thPercentile", 1.0);
+    map.put("75thPercentile", 2.0);
+    map.put("95thPercentile", 3.0);
+    map.put("98thPercentile", 4.0);
+    map.put("99thPercentile", 5.0);
+    map.put("999thPercentile", 6.0);
+    map.put("Count", 7L);
+    map.put("Max", 8.0);
+    map.put("Mean", 9.0);
+    map.put("Min", 10.0);
+    map.put("StdDev", 11.0);
+    Object mbean = Utility.createReadOnlyDynamicMBean(map);
+    register(
+        ObjectName.getInstance(
+            "kafka.network:type=RequestMetrics,request=" + request.name() + ",name=TotalTimeMs"),
+        mbean);
+
+    // act
+    TotalTimeMs totalTimeMs = request.totalTimeMs(mBeanClient);
+
+    // assert
+    assertEquals(totalTimeMs.percentile50(), 1.0);
+    assertEquals(totalTimeMs.percentile75(), 2.0);
+    assertEquals(totalTimeMs.percentile95(), 3.0);
+    assertEquals(totalTimeMs.percentile98(), 4.0);
+    assertEquals(totalTimeMs.percentile99(), 5.0);
+    assertEquals(totalTimeMs.percentile999(), 6.0);
+    assertEquals(totalTimeMs.count(), 7L);
+    assertEquals(totalTimeMs.max(), 8.0);
+    assertEquals(totalTimeMs.mean(), 9.0);
+    assertEquals(totalTimeMs.min(), 10.0);
+    assertEquals(totalTimeMs.stdDev(), 11.0);
+  }
+
+  @Test
+  void testGlobalPartitionCount() throws MalformedObjectNameException {
+    // arrange
+    Object mbean = Utility.createReadOnlyDynamicMBean(Map.of("Value", 0xcafebabe));
+    register(
+        ObjectName.getInstance("kafka.controller:type=KafkaController,name=GlobalPartitionCount"),
+        mbean);
+
+    // act
+    int integer = KafkaMetrics.TopicPartition.globalPartitionCount(mBeanClient);
+
+    // assert
+    assertEquals(0xcafebabe, integer);
+  }
+
+  @Test
+  void testUnderReplicatedPartitions() throws MalformedObjectNameException {
+    // arrange
+    Object mbean = Utility.createReadOnlyDynamicMBean(Map.of("Value", 0x55665566));
+    register(
+        ObjectName.getInstance("kafka.server:type=ReplicaManager,name=UnderReplicatedPartitions"),
+        mbean);
+
+    // act
+    int integer = KafkaMetrics.TopicPartition.underReplicatedPartitions(mBeanClient);
+
+    // assert
+    assertEquals(0x55665566, integer);
+  }
+
+  @Test
+  void testSize() throws MalformedObjectNameException {
+    // arrange
+    String topicName = "A";
+    Function<Long, Object> fMbean =
+        (Long l) -> Utility.createReadOnlyDynamicMBean(Map.of("Value", l));
+    Function<Integer, String> fName =
+        (Integer i) -> "kafka.log:type=Log,name=Size,topic=" + topicName + ",partition=" + i;
+
+    register(ObjectName.getInstance(fName.apply(1)), fMbean.apply(100L));
+    register(ObjectName.getInstance(fName.apply(2)), fMbean.apply(200L));
+    register(ObjectName.getInstance(fName.apply(4)), fMbean.apply(400L));
+    register(ObjectName.getInstance(fName.apply(5)), fMbean.apply(500L));
+
+    // act
+    Map<Integer, Long> size = KafkaMetrics.TopicPartition.size(mBeanClient, topicName);
+
+    // assert
+    assertFalse(size.containsKey(0));
+    assertEquals(100L, size.get(1));
+    assertEquals(200L, size.get(2));
+    assertFalse(size.containsKey(3));
+    assertEquals(400L, size.get(4));
+    assertEquals(500L, size.get(5));
+  }
+
+  @Test
+  void testKafkaMetricsOf() {
+    assertEquals(
+        KafkaMetrics.BrokerTopic.BytesInPerSec, KafkaMetrics.BrokerTopic.of("ByTeSiNpErSeC"));
+    assertEquals(
+        KafkaMetrics.BrokerTopic.BytesOutPerSec, KafkaMetrics.BrokerTopic.of("bytesoutpersec"));
+    assertEquals(
+        KafkaMetrics.BrokerTopic.MessagesInPerSec, KafkaMetrics.BrokerTopic.of("MessagesInPERSEC"));
+    assertThrows(IllegalArgumentException.class, () -> KafkaMetrics.BrokerTopic.of("nothing"));
+  }
+}
