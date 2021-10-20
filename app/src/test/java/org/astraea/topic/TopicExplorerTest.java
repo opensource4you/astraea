baa1ce94f727de7fb6274b5f9973e3fba291ffa4
Chia-Ping Tsai <chia7712@gmail.com>
2021-10-20 22:46:38 +0800

    Rewrite TopicExplorerTest by embedded server (#63)

--- app/src/test/java/org/astraea/topic/TopicExplorerTest.java
+++ app/src/test/java/org/astraea/topic/TopicExplorerTest.java
@@ -1,61 +1,29 @@
 package org.astraea.topic;
 
+import java.io.IOException;
 import java.util.*;
-import org.apache.kafka.common.TopicPartition;
+import java.util.concurrent.TimeUnit;
+import org.astraea.service.RequireBrokerCluster;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TopicExplorerTest {
+public class TopicExplorerTest extends RequireBrokerCluster {
 
   @Test
-  void test() {
-
-    var topicName = "topic";
-    var partition = 1000;
-    var topicPartition = new TopicPartition(topicName, partition);
-    var latestOffset = 1000L;
-    var earliestOffset = 100L;
-    var groupId = "thisIsId";
-    var groupOffset = 100;
-    var brokerId = 10;
-    var lag = 2;
-    var leader = true;
-    var inSync = true;
-    try (var admin =
-        new FakeTopicAdmin() {
-
-          @Override
-          public Map<TopicPartition, Offset> offsets(Set<String> topics) {
-            return Map.of(topicPartition, new Offset(earliestOffset, latestOffset));
-          }
-
-          @Override
-          public Map<TopicPartition, List<Group>> groups(Set<String> topics) {
-            return Map.of(
-                topicPartition,
-                List.of(new Group(groupId, OptionalLong.of(groupOffset), List.of())));
-          }
-
-          @Override
-          public Map<TopicPartition, List<Replica>> replicas(Set<String> topics) {
-            return Map.of(
-                topicPartition, List.of(new Replica(brokerId, lag, leader, inSync, false, "path")));
-          }
-        }) {
+  void testExecute() throws IOException, InterruptedException {
+    var topicName = "testExecute";
+    try (var admin = TopicAdmin.of(bootstrapServers())) {
+      admin.createTopic(topicName, 1, (short) 1);
+      // wait for topic
+      TimeUnit.SECONDS.sleep(5);
       var result = TopicExplorer.execute(admin, Set.of(topicName));
       Assertions.assertEquals(1, result.size());
       Assertions.assertEquals(topicName, result.get(0).topic);
-      Assertions.assertEquals(partition, result.get(0).partition);
-      Assertions.assertEquals(earliestOffset, result.get(0).earliestOffset);
-      Assertions.assertEquals(latestOffset, result.get(0).latestOffset);
-      Assertions.assertEquals(1, result.get(0).groups.size());
-      Assertions.assertEquals(groupId, result.get(0).groups.get(0).groupId);
-      Assertions.assertEquals(groupOffset, result.get(0).groups.get(0).offset.getAsLong());
+      Assertions.assertEquals(0, result.get(0).partition);
+      Assertions.assertEquals(0, result.get(0).earliestOffset);
+      Assertions.assertEquals(0, result.get(0).latestOffset);
+      Assertions.assertEquals(0, result.get(0).groups.size());
       Assertions.assertEquals(1, result.get(0).replicas.size());
-      Assertions.assertEquals(brokerId, result.get(0).replicas.get(0).broker);
-      Assertions.assertEquals(lag, result.get(0).replicas.get(0).lag);
-      Assertions.assertEquals(leader, result.get(0).replicas.get(0).leader);
-      Assertions.assertEquals(inSync, result.get(0).replicas.get(0).inSync);
     }
   }
 }
