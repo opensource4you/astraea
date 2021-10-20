a3452c9030a4bc5571c6da3f651c72640005e232
Chia-Ping Tsai <chia7712@gmail.com>
2021-10-20 21:59:04 +0800

    Rewrite ReplicaCollieTest by embedded server

--- app/src/test/java/org/astraea/topic/TopicAdminTest.java
+++ app/src/test/java/org/astraea/topic/TopicAdminTest.java
@@ -3,7 +3,9 @@ package org.astraea.topic;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import org.astraea.Utils;
 import org.astraea.service.RequireBrokerCluster;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,10 +13,12 @@ import org.junit.jupiter.api.Test;
 public class TopicAdminTest extends RequireBrokerCluster {
 
   @Test
-  void testPartitions() throws IOException {
+  void testPartitions() throws IOException, InterruptedException {
     var topicName = "testPartitions";
     try (var topicAdmin = TopicAdmin.of(bootstrapServers())) {
       topicAdmin.createTopic(topicName, 3);
+      // wait for syncing topic creation
+      TimeUnit.SECONDS.sleep(5);
       Assertions.assertTrue(topicAdmin.topics().contains(topicName));
       var partitions = topicAdmin.replicas(Set.of(topicName));
       Assertions.assertEquals(3, partitions.size());
@@ -32,10 +36,12 @@ public class TopicAdminTest extends RequireBrokerCluster {
   }
 
   @Test
-  void testGroups() throws IOException {
+  void testGroups() throws IOException, InterruptedException {
     var topicName = "testGroups";
     try (var topicAdmin = TopicAdmin.of(bootstrapServers())) {
       topicAdmin.createTopic(topicName, 3);
+      // wait for syncing topic creation
+      TimeUnit.SECONDS.sleep(5);
       Assertions.assertTrue(topicAdmin.topics().contains(topicName));
       var groups = topicAdmin.groups(Set.of(topicName));
       Assertions.assertEquals(0, groups.size());
@@ -43,10 +49,12 @@ public class TopicAdminTest extends RequireBrokerCluster {
   }
 
   @Test
-  void testOffsets() throws IOException {
+  void testOffsets() throws IOException, InterruptedException {
     var topicName = "testOffsets";
     try (var topicAdmin = TopicAdmin.of(bootstrapServers())) {
       topicAdmin.createTopic(topicName, 3);
+      // wait for syncing topic creation
+      TimeUnit.SECONDS.sleep(5);
       var offsets = topicAdmin.offsets(Set.of(topicName));
       Assertions.assertEquals(3, offsets.size());
       offsets
@@ -56,6 +64,26 @@ public class TopicAdminTest extends RequireBrokerCluster {
                 Assertions.assertEquals(0, offset.earliest);
                 Assertions.assertEquals(0, offset.latest);
               });
+    }
+  }
+
+  @Test
+  void testReassign() throws IOException, InterruptedException {
+    var topicName = "testReassign";
+    try (var topicAdmin = TopicAdmin.of(bootstrapServers())) {
+      topicAdmin.createTopic(topicName, 1);
+      // wait for syncing topic creation
+      TimeUnit.SECONDS.sleep(5);
+      var broker = topicAdmin.brokerIds().iterator().next();
+      topicAdmin.reassign(topicName, 0, Set.of(broker));
+      Utils.waitFor(
+          () -> {
+            var replicas = topicAdmin.replicas(Set.of(topicName));
+            var partitionReplicas = replicas.entrySet().iterator().next().getValue();
+            return replicas.size() == 1
+                && partitionReplicas.size() == 1
+                && partitionReplicas.get(0).broker == broker;
+          });
     }
   }
 }
