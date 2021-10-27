f70ed2e1c493156adb4ec3e5cee5111cf6ff8a34
Zheng-Xian Li <garyparrottt@gmail.com>
2021-10-27 15:35:10 +0800

    Implementation of KafkaMetrics (more metrics) (#66)

---
+++ app/src/main/java/org/astraea/metrics/java/metrics/JvmMemory.java
@@ -0,0 +1,47 @@
+package org.astraea.metrics.java.metrics;
+
+import java.lang.management.MemoryUsage;
+import org.astraea.metrics.jmx.BeanObject;
+import org.astraea.metrics.kafka.metrics.modifiers.HasJvmMemory;
+
+public class JvmMemory implements HasJvmMemory {
+
+  private final BeanObject beanObject;
+  private MemoryUsage heapMemoryUsage;
+  private MemoryUsage nonHeapMemoryUsage;
+
+  @Override
+  public MemoryUsage heapMemoryUsage() {
+    // override the default implementation to avoid creating excessive objects
+    if (heapMemoryUsage == null) heapMemoryUsage = HasJvmMemory.super.heapMemoryUsage();
+    return heapMemoryUsage;
+  }
+
+  @Override
+  public MemoryUsage nonHeapMemoryUsage() {
+    // override the default implementation to avoid creating excessive objects
+    if (nonHeapMemoryUsage == null) nonHeapMemoryUsage = HasJvmMemory.super.nonHeapMemoryUsage();
+    return nonHeapMemoryUsage;
+  }
+
+  public JvmMemory(BeanObject beanObject) {
+    this.beanObject = beanObject;
+  }
+
+  @Override
+  public BeanObject beanObject() {
+    return beanObject;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb =
+        new StringBuilder()
+            .append("    HeapMemoryUsage: ")
+            .append(heapMemoryUsage())
+            .append(System.lineSeparator())
+            .append("    NonHeapMemoryUsage")
+            .append(nonHeapMemoryUsage());
+    return "JvmMemory {\n" + sb + "\n}";
+  }
+}
