6cfefe4426762e982c85e65d463387cd2fa8afeb
Chia-Ping Tsai <chia7712@gmail.com>
2021-10-27 15:42:38 +0800

    add more tests

--- app/src/test/java/org/astraea/service/RequireJmxServerTest.java
+++ app/src/test/java/org/astraea/service/RequireJmxServerTest.java
@@ -2,16 +2,25 @@ package org.astraea.service;
 
 import org.astraea.metrics.jmx.BeanQuery;
 import org.astraea.metrics.jmx.MBeanClient;
+import org.astraea.metrics.kafka.KafkaMetrics;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RequireJmxServerTest extends RequireBrokerCluster {
 
   @Test
-  void testJmxServer() throws Exception {
+  void testQueryBeans() throws Exception {
     try (var client = new MBeanClient(jmxServiceURL())) {
       var result = client.queryBeans(BeanQuery.all());
       Assertions.assertFalse(result.isEmpty());
+    }
+  }
+
+  @Test
+  void testMemory() throws Exception {
+    try (var client = new MBeanClient(jmxServiceURL())) {
+      var memory = KafkaMetrics.Host.jvmMemory(client);
+      Assertions.assertNotEquals(0, memory.heapMemoryUsage().getMax());
     }
   }
 }
