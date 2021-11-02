8f382c8fbb02c403657308e6ebe5e9ce4531e3a3
chinghongfang <fjh7777@gmail.com>
2021-11-02 23:38:03 +0800

    Add javadoc

--- app/src/main/java/org/astraea/consumer/ConsumerRebalanceListener.java
+++ app/src/main/java/org/astraea/consumer/ConsumerRebalanceListener.java
@@ -8,7 +8,12 @@ import org.apache.kafka.common.TopicPartition;
 @FunctionalInterface
 public interface ConsumerRebalanceListener {
 
-  
+  /**
+   * A callback method the user can implement to provide handling of customized offsets on
+   * completion of a successful partition re-assignment. See {@link
+   * org.apache.kafka.clients.consumer.ConsumerRebalanceListener#onPartitionsAssigned(Collection)}
+   * for more detail.
+   */
   void onPartitionAssigned(Set<TopicPartition> partitions);
 
   static org.apache.kafka.clients.consumer.ConsumerRebalanceListener of(
