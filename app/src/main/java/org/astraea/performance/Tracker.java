be80c576a54a770cbf8773d9e0902a67f3b1194b
Chia-Ping Tsai <chia7712@gmail.com>
2021-11-02 10:16:19 +0800

    Correct the average bytes (#92)

--- app/src/main/java/org/astraea/performance/Tracker.java
+++ app/src/main/java/org/astraea/performance/Tracker.java
@@ -18,26 +18,28 @@ public class Tracker implements ThreadPool.Executor {
   @Override
   public State execute() throws InterruptedException {
     long completed = 0;
-    long bytes = 0L;
+    double bytes = 0;
     long max = 0;
     long min = Long.MAX_VALUE;
 
     /* producer */
     for (Metrics data : producerData) {
       completed += data.num();
-      bytes += data.bytesThenReset();
-      if (max < data.max()) max = data.max();
-      if (min > data.min()) min = data.min();
+      bytes += data.avgBytes();
+      max = Math.max(max, data.max());
+      min = Math.min(min, data.min());
     }
     if (completed == 0) {
       Thread.sleep(1000);
       return State.RUNNING;
     }
     System.out.printf("producers完成度: %.2f%%%n", ((double) completed * 100.0 / (double) records));
-    System.out.printf("  輸出%.3fMB/second%n", ((double) bytes / (1 << 20)));
+    System.out.printf("  輸出%.3fMB/second%n", bytes);
     System.out.println("  發送max latency: " + max + "ms");
     System.out.println("  發送mim latency: " + min + "ms");
     for (int i = 0; i < producerData.size(); ++i) {
+      System.out.printf(
+          "  producer[%d]的發送average bytes: %.3fMB%n", i, producerData.get(i).avgBytes());
       System.out.printf(
           "  producer[%d]的發送average latency: %.3fms%n", i, producerData.get(i).avgLatency());
     }
@@ -48,15 +50,17 @@ public class Tracker implements ThreadPool.Executor {
     min = Long.MAX_VALUE;
     for (Metrics data : consumerData) {
       completed += data.num();
-      bytes += data.bytesThenReset();
-      if (max < data.max()) max = data.max();
-      if (min > data.min()) min = data.min();
+      bytes += data.avgBytes();
+      max = Math.max(max, data.max());
+      min = Math.min(min, data.min());
     }
     System.out.printf("consumer完成度: %.2f%%%n", ((double) completed * 100.0 / (double) records));
-    System.out.printf("  輸入%.3fMB/second%n", ((double) bytes / (1 << 20)));
+    System.out.printf("  輸入%.3fMB/second%n", bytes);
     System.out.println("  端到端max latency: " + max + "ms");
     System.out.println("  端到端mim latency: " + min + "ms");
     for (int i = 0; i < consumerData.size(); ++i) {
+      System.out.printf(
+          "  consumer[%d]的端到端average bytes: %.3fMB%n", i, consumerData.get(i).avgBytes());
       System.out.printf(
           "  consumer[%d]的端到端average latency: %.3fms%n", i, consumerData.get(i).avgLatency());
     }
