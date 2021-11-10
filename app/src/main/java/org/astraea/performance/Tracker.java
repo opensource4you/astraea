50aef3439976e73bb7226ff14c840013f18c0028
Chia-Ping Tsai <chia7712@gmail.com>
2021-11-10 17:26:05 +0800

    No consumer causes incorrect console output (#100)

--- app/src/main/java/org/astraea/performance/Tracker.java
+++ app/src/main/java/org/astraea/performance/Tracker.java
@@ -17,22 +17,27 @@ public class Tracker implements ThreadPool.Executor {
 
   @Override
   public State execute() throws InterruptedException {
-    long completed = 0;
-    double bytes = 0;
-    long max = 0;
-    long min = Long.MAX_VALUE;
+    if (logProducers() & logConsumers()) return State.DONE;
+    // 等待1秒，再抓資料輸出
+    Thread.sleep(1000);
+    return State.RUNNING;
+  }
 
-    /* producer */
+  private boolean logProducers() {
+    var completed = 0L;
+    var bytes = 0D;
+    var max = 0L;
+    var min = Long.MAX_VALUE;
+
     for (Metrics data : producerData) {
       completed += data.num();
       bytes += data.avgBytes();
       max = Math.max(max, data.max());
       min = Math.min(min, data.min());
     }
-    if (completed == 0) {
-      Thread.sleep(1000);
-      return State.RUNNING;
-    }
+
+    if (completed == 0) return false;
+
     System.out.printf("producers完成度: %.2f%%%n", ((double) completed * 100.0 / (double) records));
     System.out.printf("  輸出%.3fMB/second%n", bytes);
     System.out.println("  發送max latency: " + max + "ms");
@@ -43,21 +48,26 @@ public class Tracker implements ThreadPool.Executor {
       System.out.printf(
           "  producer[%d]的發送average latency: %.3fms%n", i, producerData.get(i).avgLatency());
     }
-    if (consumerData.isEmpty()) {
-      if (completed >= records) return State.DONE;
-      else return State.RUNNING;
-    }
-    /* consumer */
-    completed = 0;
-    bytes = 0L;
-    max = 0;
-    min = Long.MAX_VALUE;
+    System.out.println("\n");
+    return completed >= records;
+  }
+
+  private boolean logConsumers() {
+    // there is no consumer, so we just complete this log.
+    if (consumerData.isEmpty()) return true;
+    var completed = 0;
+    var bytes = 0D;
+    var max = 0L;
+    var min = Long.MAX_VALUE;
     for (Metrics data : consumerData) {
       completed += data.num();
       bytes += data.avgBytes();
       max = Math.max(max, data.max());
       min = Math.min(min, data.min());
     }
+
+    if (completed == 0) return false;
+
     System.out.printf("consumer完成度: %.2f%%%n", ((double) completed * 100.0 / (double) records));
     System.out.printf("  輸入%.3fMB/second%n", bytes);
     System.out.println("  端到端max latency: " + max + "ms");
@@ -68,12 +78,7 @@ public class Tracker implements ThreadPool.Executor {
       System.out.printf(
           "  consumer[%d]的端到端average latency: %.3fms%n", i, consumerData.get(i).avgLatency());
     }
-
     System.out.println("\n");
-    // The consumer has consumed the target records
-    if (completed >= records) return State.DONE;
-    // 等待1秒，再抓資料輸出
-    Thread.sleep(1000);
-    return State.RUNNING;
+    return completed >= records;
   }
 }
