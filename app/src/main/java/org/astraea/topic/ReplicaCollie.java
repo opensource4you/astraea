375e1bffb91858c3f7620443cc5b452c66c8e346
qoo332001 <sean0651101@gmail.com>
2021-11-28 14:18:44 +0800

    save

--- app/src/main/java/org/astraea/topic/ReplicaCollie.java
+++ app/src/main/java/org/astraea/topic/ReplicaCollie.java
@@ -9,7 +9,7 @@ import org.astraea.argument.ArgumentUtil;
 import org.astraea.argument.BasicArgumentWithPropFile;
 
 public class ReplicaCollie {
-  static Map<TopicPartition, Map.Entry<Set<Integer>, Set<Integer>>> execute(
+  static void execute(
       TopicAdmin admin, Argument args) {
     var topics = args.topics.isEmpty() ? admin.topicNames() : args.topics;
     var allBrokers = admin.brokerIds();
@@ -28,12 +28,12 @@ public class ReplicaCollie {
                   .map(String::valueOf)
                   .collect(Collectors.joining(","))
               + " are nonexistent");
-    if (!args.partition.isEmpty() && !allTopicsPartitions.containsAll(args.partition))
+    if (!args.partitions.isEmpty() && !allTopicsPartitions.containsAll(args.partitions))
       throw new IllegalArgumentException(
           "Topic "
               + topics.iterator().next()
               + " does not exist partition: "
-              + args.partition.toString());
+              + args.partitions.toString());
     var targetBrokers = args.toBrokers.isEmpty() ? allBrokers : args.toBrokers;
     var result =
         new TreeMap<TopicPartition, Map.Entry<Set<Integer>, Set<Integer>>>(
@@ -42,7 +42,7 @@ public class ReplicaCollie {
         new TreeMap<TopicPartition, Map.Entry<Set<String>, Set<String>>>(
             Comparator.comparing(TopicPartition::topic).thenComparing(TopicPartition::partition));
     admin.replicas(topics).entrySet().stream()
-        .filter(tp -> args.partition.contains(tp.getKey().partition()) || args.partition.isEmpty())
+        .filter(tp -> args.partitions.contains(tp.getKey().partition()) || args.partitions.isEmpty())
         .collect(Collectors.toSet())
         .forEach(
             (tp) -> {
@@ -73,54 +73,54 @@ public class ReplicaCollie {
               }
             });
     if (result.isEmpty()) crossBroker = false;
+    printMoved(result);
     result.forEach(
         (tp, assignments) -> {
           if (!args.verify)
             admin.migrator().partition(tp.topic(), tp.partition()).moveTo(assignments.getValue());
         });
-    if (path != null && !args.partition.isEmpty()) {
-      if (!crossBroker && !admin.brokerFolders(allBrokers).get(fromBroker).contains(path)
-          || crossBroker
+    if (path != null && !args.partitions.isEmpty()) {
+      if (crossBroker
               && !admin
                   .brokerFolders(allBrokers)
                   .get(targetBrokers.iterator().next())
                   .contains(path))
-        throw new IllegalArgumentException("path: " + path + " is not in broker" + fromBroker);
+        throw new IllegalArgumentException("path: " + path + " is not in broker" + args.toBrokers.iterator().next());
       admin.replicas(topics).entrySet().stream()
           .filter(
-              t ->
-                  t.getKey().topic().equals(topics.iterator().next())
-                      && args.partition.contains(t.getKey().partition()))
+              t ->t.getValue().get(0).broker()==fromBroker && t.getKey().topic().equals(topics.iterator().next())
+                      && args.partitions.contains(t.getKey().partition()))
           .collect(Collectors.toList())
           .forEach(
               (tp) -> {
                 var currentPath =
-                    tp.getValue().stream().map(Replica::path).collect(Collectors.toSet());
-                if (tp.getValue().get(0).broker() == 0)
+                        Set.of(tp.getValue().get(0).path());
+                if (tp.getValue().get(0).broker() == targetBrokers.iterator().next())
                   if (topics.iterator().next().equals(tp.getKey().topic()))
                     if (!currentPath.equals(args.path))
                       result2.put(tp.getKey(), Map.entry(currentPath, args.path));
               });
+      printMoved(result2);
     }
-    return result;
+  }
+  static <T>void printMoved(Map<TopicPartition, Map.Entry<Set<T>, Set<T>>> result){
+          result.forEach(
+            (tp, assignments) ->
+                    System.out.println(
+                            "topic: "
+                                    + tp.topic()
+                                    + ", partition: "
+                                    + tp.partition()
+                                    + " before: "
+                                    + assignments.getKey()
+                                    + " after: "
+                                    + assignments.getValue()));
   }
 
   public static void main(String[] args) throws IOException {
     var argument = ArgumentUtil.parseArgument(new Argument(), args);
     try (var admin = TopicAdmin.of(argument.props())) {
-      var a = ReplicaCollie.<String>execute(admin, argument);
-      var b = ReplicaCollie.<Integer>execute(admin, argument);
-      a.forEach(
-          (tp, assignments) ->
-              System.out.println(
-                  "topic: "
-                      + tp.topic()
-                      + ", partition: "
-                      + tp.partition()
-                      + " before: "
-                      + assignments.getKey()
-                      + " after: "
-                      + assignments.getValue()));
+      execute(admin, argument);
     }
   }
 
@@ -148,11 +148,11 @@ public class ReplicaCollie {
     Set<Integer> toBrokers = Collections.emptySet();
 
     @Parameter(
-        names = {"--partition"},
+        names = {"--partitions"},
         description = "A partition that will be moved",
         validateWith = ArgumentUtil.NotEmptyString.class,
         converter = ArgumentUtil.IntegerSetConverter.class)
-    Set<Integer> partition = Collections.emptySet();
+    Set<Integer> partitions = Collections.emptySet();
 
     @Parameter(
         names = {"--path"},
