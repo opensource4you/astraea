6c83dfe08dcd4c27965ef023443bf2dc0d4b8773
Chia-Ping Tsai <chia7712@gmail.com>
2021-12-04 16:22:18 +0800

    The address of jmx server should not use '127.0.0.1'

--- app/src/test/java/org/astraea/service/RequireJmxServerTest.java
+++ app/src/test/java/org/astraea/service/RequireJmxServerTest.java
@@ -1,5 +1,6 @@
 package org.astraea.service;
 
+import java.util.Set;
 import org.astraea.metrics.jmx.BeanQuery;
 import org.astraea.metrics.jmx.MBeanClient;
 import org.astraea.metrics.kafka.KafkaMetrics;
@@ -31,6 +32,17 @@ public class RequireJmxServerTest extends RequireBrokerCluster {
     try (client) {
       var memory = KafkaMetrics.Host.jvmMemory(client);
       Assertions.assertNotEquals(0, memory.heapMemoryUsage().getMax());
+    }
+  }
+
+  @Test
+  void testHost() {
+    var legalChars = Set.of('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.');
+    var address = jmxServiceURL().getHost();
+    Assertions.assertNotEquals("127.0.0.1", address);
+    Assertions.assertNotEquals("0.0.0.0", address);
+    for (var i = 0; i < address.length(); i++) {
+      Assertions.assertTrue(legalChars.contains(address.charAt(i)));
     }
   }
 }
