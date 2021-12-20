40d70a80f7b5ef4d062820d68eeae6a7eaa9d693
Chia-Ping Tsai <chia7712@gmail.com>
2021-12-21 03:18:12 +0800

    Pre-install dependencies to spark image; Skip building existent docker image (#158)

--- docker/start_spark.sh
+++ docker/start_spark.sh
@@ -7,7 +7,10 @@ function showHelp() {
   echo "ENV: "
   echo "    REPO=astraea/spark               set the docker repo"
   echo "    VERSION=3.1.2                    set version of spark distribution"
+  echo "    DELTA_VERSION=1.0.0              set version of delta distribution"
+  echo "    PY_KAFKA_VERSION=1.7.0           set version of confluent kafka distribution"
   echo "    RUN=false                        set false if you want to build image only"
+  echo "    PYTHON_DEPS=delta-spark=1.0.0    those dependencies will be pre-installed in the docker image"
 }
 
 if [[ "$(which docker)" == "" ]]; then
@@ -57,12 +60,22 @@ image_name="$repo:$version"
 run_container=${RUN:-true}
 spark_port="$(($(($RANDOM % 10000)) + 10000))"
 spark_ui_port="$(($(($RANDOM % 10000)) + 10000))"
+ivy_version=2.5.0
+delta_version=${DELTA_VERSION:-1.0.0}
+py_kafka_version=${PY_KAFKA_VERSION:-1.7.0}
+# hardcode version to avoid NPE (see https://issues.apache.org/jira/browse/HADOOP-16410)
+hadoop_version=3.2.2
+docker_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+dockerfile=$docker_dir/spark.dockerfile
 
-docker build -t $image_name - <<Dockerfile
+echo "# this dockerfile is generated dynamically
 FROM ubuntu:20.04
 
+# Do not ask for confirmations when running apt-get, etc.
+ENV DEBIAN_FRONTEND noninteractive
+
 # install tools
-RUN apt-get update && apt-get upgrade -y && apt-get install -y openjdk-11-jdk wget python3
+RUN apt-get update && apt-get upgrade -y && apt-get install -y openjdk-11-jdk wget python3 python3-pip unzip
 
 # add user
 RUN groupadd $spark_user && useradd -ms /bin/bash -g $spark_user $spark_user
@@ -70,19 +83,31 @@ RUN groupadd $spark_user && useradd -ms /bin/bash -g $spark_user $spark_user
 # change user
 USER $spark_user
 
+# pre-install python dependencies
+RUN pip3 install confluent-kafka==$py_kafka_version delta-spark==$delta_version pyspark==$version
+
+# install java dependencies
+WORKDIR /tmp
+RUN wget https://dlcdn.apache.org//ant/ivy/${ivy_version}/apache-ivy-${ivy_version}-bin.zip
+RUN unzip apache-ivy-${ivy_version}-bin.zip
+WORKDIR apache-ivy-${ivy_version}
+RUN java -jar ./ivy-${ivy_version}.jar -dependency io.delta delta-core_2.12 $delta_version
+RUN java -jar ./ivy-${ivy_version}.jar -dependency org.apache.spark spark-sql-kafka-0-10_2.12 $version
+RUN java -jar ./ivy-${ivy_version}.jar -dependency org.apache.spark spark-token-provider-kafka-0-10_2.12 $version
+RUN java -jar ./ivy-${ivy_version}.jar -dependency org.apache.hadoop hadoop-azure $hadoop_version
+
 # download spark
 WORKDIR /tmp
 RUN wget https://archive.apache.org/dist/spark/spark-${version}/spark-${version}-bin-hadoop3.2.tgz
 RUN mkdir /home/$spark_user/spark
 RUN tar -zxvf spark-${version}-bin-hadoop3.2.tgz -C /home/$spark_user/spark --strip-components=1
-ENV SPARK_MASTER_WEBUI_PORT=$spark_ui_port
-ENV SPARK_WORKER_WEBUI_PORT=$spark_ui_port
-ENV SPARK_MASTER_PORT=$spark_port
-ENV SPARK_WORKER_PORT=$spark_port
-ENV SPARK_NO_DAEMONIZE=true
 WORKDIR /home/$spark_user/spark
+" > "$dockerfile"
 
-Dockerfile
+# build image only if the image does not exist locally
+if [[ "$(docker images -q $image_name 2> /dev/null)" == "" ]]; then
+  docker build -t $image_name -f "$dockerfile" "$docker_dir"
+fi
 
 if [[ "$run_container" != "true" ]]; then
   echo "docker image: $image_name is created"
@@ -102,6 +127,9 @@ fi
 
 if [[ -n "$master_url" ]]; then
   docker run -d \
+    -e SPARK_WORKER_WEBUI_PORT=$spark_ui_port \
+    -e SPARK_WORKER_PORT=$spark_port \
+    -e SPARK_NO_DAEMONIZE=true \
     --name "$worker_name" \
     --network host \
     $image_name ./sbin/start-worker.sh "$master_url"
@@ -112,6 +140,9 @@ if [[ -n "$master_url" ]]; then
   echo "================================================="
 else
   docker run -d \
+    -e SPARK_MASTER_WEBUI_PORT=$spark_ui_port \
+    -e SPARK_MASTER_PORT=$spark_port \
+    -e SPARK_NO_DAEMONIZE=true \
     --name "$master_name" \
     --network host \
     $image_name ./sbin/start-master.sh
--- docker/submit_spark_job.sh
+++ docker/submit_spark_job.sh
@@ -7,6 +7,8 @@ function showHelp() {
   echo "ENV: "
   echo "    REPO=astraea/spark-job             set the docker repo"
   echo "    VERSION=3.1.2                      set version of spark distribution"
+  echo "    DELTA_VERSION=1.0.0                set version of delta distribution"
+  echo "    PY_KAFKA_VERSION=1.7.0             set version of confluent kafka distribution"
   echo "    RUN=false                          set false if you want to build image only"
   echo "    MOUNT=host_path:container_path     mount the host path to container path"
 }
@@ -42,12 +44,22 @@ version=${REVISION:-${VERSION:-3.1.2}}
 repo=${REPO:-astraea/spark-job}
 image_name="$repo:$version"
 run_container=${RUN:-true}
+ivy_version=2.5.0
+delta_version=${DELTA_VERSION:-1.0.0}
+py_kafka_version=${PY_KAFKA_VERSION:-1.7.0}
+# hardcode version to avoid NPE (see https://issues.apache.org/jira/browse/HADOOP-16410)
+hadoop_version=3.2.2
+docker_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+dockerfile=$docker_dir/spark-job.dockerfile
 
-docker build -t $image_name - <<Dockerfile
+echo "# this dockerfile is generated dynamically
 FROM ubuntu:20.04
 
+# Do not ask for confirmations when running apt-get, etc.
+ENV DEBIAN_FRONTEND noninteractive
+
 # install tools
-RUN apt-get update && apt-get upgrade -y && apt-get install -y openjdk-11-jdk wget python3
+RUN apt-get update && apt-get upgrade -y && apt-get install -y openjdk-11-jdk wget python3 python3-pip unzip
 
 # add user
 RUN groupadd $spark_user && useradd -ms /bin/bash -g $spark_user $spark_user
@@ -55,14 +67,31 @@ RUN groupadd $spark_user && useradd -ms /bin/bash -g $spark_user $spark_user
 # change user
 USER $spark_user
 
+# pre-install python dependencies
+RUN pip3 install confluent-kafka==$py_kafka_version delta-spark==$delta_version pyspark==$version
+
+# install java dependencies
+WORKDIR /tmp
+RUN wget https://dlcdn.apache.org//ant/ivy/${ivy_version}/apache-ivy-${ivy_version}-bin.zip
+RUN unzip apache-ivy-${ivy_version}-bin.zip
+WORKDIR apache-ivy-${ivy_version}
+RUN java -jar ./ivy-${ivy_version}.jar -dependency io.delta delta-core_2.12 $delta_version
+RUN java -jar ./ivy-${ivy_version}.jar -dependency org.apache.spark spark-sql-kafka-0-10_2.12 $version
+RUN java -jar ./ivy-${ivy_version}.jar -dependency org.apache.spark spark-token-provider-kafka-0-10_2.12 $version
+RUN java -jar ./ivy-${ivy_version}.jar -dependency org.apache.hadoop hadoop-azure $hadoop_version
+
 # download spark
 WORKDIR /tmp
 RUN wget https://archive.apache.org/dist/spark/spark-${version}/spark-${version}-bin-hadoop3.2.tgz
 RUN mkdir /home/$spark_user/spark
 RUN tar -zxvf spark-${version}-bin-hadoop3.2.tgz -C /home/$spark_user/spark --strip-components=1
 WORKDIR /home/$spark_user/spark
+" > "$dockerfile"
 
-Dockerfile
+# build image only if the image does not exist locally
+if [[ "$(docker images -q $image_name 2> /dev/null)" == "" ]]; then
+  docker build -t $image_name -f "$dockerfile" "$docker_dir"
+fi
 
 if [[ "$run_container" != "true" ]]; then
   echo "docker image: $image_name is created"
@@ -115,15 +144,11 @@ if [[ -n "$MOUNT" ]]; then
   docker run \
     --network host \
     $hostFolderConfigs \
-    -v "$HOME"/.local/lib:/home/$spark_user/.local/lib:ro \
-    -v "$HOME"/.ivy2:/home/$spark_user/.ivy2 \
     -v "$py_folder":"$py_folder":ro \
     $image_name ./bin/spark-submit $args
 else
   docker run \
     --network host \
-    -v "$HOME"/.local/lib:/home/$spark_user/.local/lib:ro \
-    -v "$HOME"/.ivy2:/home/$spark_user/.ivy2 \
     -v "$py_folder":"$py_folder":ro \
     $image_name ./bin/spark-submit $args
 fi
