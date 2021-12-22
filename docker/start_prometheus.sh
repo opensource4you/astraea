c76d7b9c3098008de91db36f2abb21f32545ed99
Zheng-Xian Li <garyparrottt@gmail.com>
2021-12-22 11:21:53 +0800

    script for setting up some observability utilities
    * Grafana
    * Prometheus
    * Node Exporter

--- docker/start_prometheus.sh
+++ docker/start_prometheus.sh
@@ -3,7 +3,12 @@
 # =============================[functions]=============================
 
 function showHelp() {
-  echo "Usage: [targets]"
+  echo "Usage: start_prometheus.sh [ COMMAND ]"
+  echo "COMMAND: "
+  echo "    start <kafka-broker-jmx-addresses> <node-exporter-addresses>    create/start the prometheus docker instance"
+  echo "    refresh <kafka-broker-jmx-addresses> <node-exporter-addresses>  refresh and apply the prometheus config"
+  echo "    stop                                                            stop the prometheus container"
+  echo "    help                                                            show this dialog"
 }
 
 # ===============================[checks]===============================
@@ -13,7 +18,7 @@ if [[ "$(which docker)" == "" ]]; then
   exit 2
 fi
 
-if [[ "$(which ipconfig)" != "" ]]; then
+if [[ "$(which ipconfig 2> /dev/null)" != "" ]]; then
   address=$(ipconfig getifaddr en0)
 else
   address=$(hostname -i)
@@ -26,46 +31,126 @@ fi
 
 # =================================[main]=================================
 
-if [[ "$1" == "" ]] || [[ "$1" == "help" ]]; then
-  showHelp
-  exit 0
-fi
+function wrap_address() {
+  _value=""
+  IFS=',' read -ra ADDR <<< "$1"
+  for i in "${ADDR[@]}"; do
+    if [[ "$_value" == "" ]]; then
+      _value="'$i'"
+    else
+      _value="$_value,'$i'"
+    fi
+  done
+  echo "$_value"
+}
 
-targets=""
-IFS=',' read -ra ADDR <<< "$1"
-for i in "${ADDR[@]}"; do
-  if [[ "$targets" == "" ]]; then
-    targets="'$i'"
-  else
-    targets="$targets,'$i'"
-  fi
-done
-echo "$targets"
-
+prometheus_port="${PROMETHEUS_PORT:-9090}"
 image_name=prom/prometheus
-prometheus_port="$(($(($RANDOM % 10000)) + 10000))"
+container_name="prometheus-${prometheus_port}"
 file=/tmp/prometheus-${prometheus_port}.yml
+scrape_interval="5s"
 
-cat <<EOT > "$file"
+function write_config() {
+  kafka_jmx_addresses="$(wrap_address "$1")"
+  node_exporter_addresses="$(wrap_address "$2")"
+
+  cat <<EOT > "$file"
 global:
   scrape_interval: 15s
   external_labels:
-    monitor: 'kafka-monitor'
+    monitor: 'prometheus'
 
 scrape_configs:
-  - job_name: 'brokers'
-    scrape_interval: 5s
+
+  - job_name: 'kafka'
+    scrape_interval: $scrape_interval
     static_configs:
-      - targets: [$targets]
+      - targets: [$kafka_jmx_addresses]
 
+  - job_name: 'node'
+    scrape_interval: $scrape_interval
+    static_configs:
+      - targets: [$node_exporter_addresses]
 EOT
+}
 
-docker run -d \
-    -p $prometheus_port:9090 \
-    -v $file:/etc/prometheus/prometheus.yml \
-    prom/prometheus
+function is_prometheus_exists() {
+  docker ps -a | grep "$container_name" > /dev/null
+  if [ $? -eq 0 ]; then echo "yes"; else echo "no"; fi
+}
 
-echo "================================================="
-echo "config file: $file"
-echo "prometheus address: http://${address}:$prometheus_port"
-echo "================================================="
+function is_prometheus_running() {
+  docker ps | grep "$container_name" > /dev/null
+  if [ $? -eq 0 ]; then echo "yes"; else echo "no"; fi
+}
+
+function info() {
+  # shellcheck disable=SC2068
+  echo "[INFO]" $@
+}
+
+function refresh_config_from_file() {
+  info "Refresh prometheus configuration from config file $file"
+  docker kill --signal="SIGHUP" "$container_name"
+}
+
+function refresh_config() {
+  info "Refresh prometheus configuration"
+
+  write_config "$1" "$2"
+  docker kill --signal="SIGHUP" "$container_name"
+}
+
+function stop() {
+  info "Stop prometheus"
+  docker stop "$container_name"
+}
+
+function main() {
+  if [ "$(is_prometheus_running)" == "yes" ]; then
+    info "Prometheus is already running" at "http://$address:$prometheus_port"
+    showHelp
+    exit 0
+  fi
+
+  write_config "$1" "$2"
+
+  if [ "$(is_prometheus_exists)" == "yes" ]; then
+    info "Start existing prometheus instance"
+    docker start "$container_name"
+  else
+    info "Create a new prometheus docker instance"
+    docker run -d \
+        --name "$container_name" \
+        -p "$prometheus_port:9090" \
+        -v "$file:/etc/prometheus/prometheus.yml" \
+        $image_name
+  fi
+
+
+  info "================================================="
+  info "config file: $file"
+  info "prometheus address: http://${address}:$prometheus_port"
+  info "command to run grafana at this host: ./docker/grafana.sh start"
+  info "command to add prometheus to grafana datasource: ./docker/grafana.sh add_prom_source <USERNAME>:<PASSWORD> Prometheus http://$address:$prometheus_port"
+  info "================================================="
+}
+
+
+# shellcheck disable=SC2199
+if [[ "$1" == "start" ]]; then
+  # shellcheck disable=SC2068
+  main "$2" "$3"
+elif [[ "$1" == "refresh" ]] && [[ "$2" != "" ]]; then
+  # shellcheck disable=SC2068
+  refresh_config "$2" "$3"
+elif [[ "$1" == "refresh" ]]; then
+  refresh_config_from_file
+elif [[ "$1" == "stop" ]]; then
+  stop
+elif [[ "$1" == "help" ]]; then
+  showHelp
+else
+  info Unknown argument: "$@"
+  showHelp
+fi
