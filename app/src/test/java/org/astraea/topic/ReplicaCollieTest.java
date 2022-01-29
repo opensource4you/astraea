82cc3ed3416a752faeda2f11e5fce35b1dea06d4
Xiang-Jun Sun <sean0651101@gmail.com>
2022-01-30 01:21:16 +0800

    Enhance ReplicaCollie to support migrate replica to another folder (#133)

--- app/src/test/java/org/astraea/topic/ReplicaCollieTest.java
+++ app/src/test/java/org/astraea/topic/ReplicaCollieTest.java
@@ -3,7 +3,10 @@ package org.astraea.topic;
 import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import org.apache.kafka.common.TopicPartition;
@@ -27,6 +30,93 @@ public class ReplicaCollieTest extends RequireBrokerCluster {
     test(false);
   }
 
+  @Test
+  @DisabledOnOs(WINDOWS)
+  void testBrokerMigrator() throws IOException, InterruptedException {
+    var topicName = "ReplicaCollieTest";
+    try (var topicAdmin = TopicAdmin.of(bootstrapServers())) {
+      topicAdmin
+          .creator()
+          .topic(topicName)
+          .numberOfPartitions(1)
+          .numberOfReplicas((short) 2)
+          .create();
+      // wait for topic creation
+      TimeUnit.SECONDS.sleep(5);
+      var partitionReplicas = topicAdmin.replicas(Set.of(topicName));
+      Assertions.assertEquals(1, partitionReplicas.size());
+      var brokerSource =
+          partitionReplicas.get(new TopicPartition(topicName, 0)).stream()
+              .map(Replica::broker)
+              .collect(Collectors.toSet());
+      var brokerSink =
+          topicAdmin.brokerIds().stream().filter(b -> !brokerSource.contains(b)).iterator().next();
+      TreeMap<TopicPartition, Map.Entry<Set<Integer>, Set<Integer>>> brokerMigrate =
+          new TreeMap<>(
+              Comparator.comparing(TopicPartition::topic).thenComparing(TopicPartition::partition));
+      brokerMigrate.put(
+          new TopicPartition(topicName, 0), Map.entry(brokerSource, Set.of(brokerSink)));
+      Assertions.assertEquals(
+          topicAdmin.replicas(Set.of(topicName)).get(new TopicPartition(topicName, 0)).size(), 2);
+      ReplicaCollie.brokerMigrator(brokerMigrate, topicAdmin);
+      Utils.waitFor(
+          () ->
+              topicAdmin.replicas(Set.of(topicName)).get(new TopicPartition(topicName, 0)).size()
+                      == 1
+                  && topicAdmin
+                          .replicas(Set.of(topicName))
+                          .get(new TopicPartition(topicName, 0))
+                          .get(0)
+                          .broker()
+                      == brokerSink);
+    }
+  }
+
+  @Test
+  @DisabledOnOs(WINDOWS)
+  void testPathMigrator() throws IOException, InterruptedException {
+    var topicName = "ReplicaCollieTest";
+    try (var topicAdmin = TopicAdmin.of(bootstrapServers())) {
+      topicAdmin
+          .creator()
+          .topic(topicName)
+          .numberOfPartitions(1)
+          .numberOfReplicas((short) 2)
+          .create();
+      // wait for topic creation
+      TimeUnit.SECONDS.sleep(5);
+      var partitionReplicas = topicAdmin.replicas(Set.of(topicName));
+      Assertions.assertEquals(1, partitionReplicas.size());
+      var brokerSource = partitionReplicas.get(new TopicPartition(topicName, 0)).get(0).broker();
+      var pathSource =
+          partitionReplicas.get(new TopicPartition(topicName, 0)).stream()
+              .map(Replica::path)
+              .collect(Collectors.toSet());
+      var pathSink =
+          topicAdmin.brokerFolders(Set.of(brokerSource)).get(brokerSource).stream()
+              .filter(p -> !pathSource.contains(p))
+              .iterator()
+              .next();
+      TreeMap<TopicPartition, Map.Entry<Set<String>, Set<String>>> pathMigrate =
+          new TreeMap<>(
+              Comparator.comparing(TopicPartition::topic).thenComparing(TopicPartition::partition));
+      pathMigrate.put(new TopicPartition(topicName, 0), Map.entry(pathSource, Set.of(pathSink)));
+      Assertions.assertFalse(pathSource.contains(pathSink));
+      Assertions.assertEquals(
+          topicAdmin.replicas(Set.of(topicName)).get(new TopicPartition(topicName, 0)).size(), 2);
+      ReplicaCollie.pathMigrator(pathMigrate, topicAdmin, brokerSource);
+      Utils.waitFor(
+          () ->
+              topicAdmin
+                  .replicas(Set.of(topicName))
+                  .get(new TopicPartition(topicName, 0))
+                  .get(0)
+                  .path()
+                  .equals(pathSink));
+      topicAdmin.replicas(Set.of(topicName)).get(new TopicPartition(topicName, 0));
+    }
+  }
+
   private void test(boolean verify) throws IOException, InterruptedException {
     var topicName = "ReplicaCollieTest-" + verify;
     try (var topicAdmin = TopicAdmin.of(bootstrapServers())) {
@@ -45,18 +135,22 @@ public class ReplicaCollieTest extends RequireBrokerCluster {
               .filter(Replica::isCurrent)
               .collect(Collectors.toList());
       Assertions.assertEquals(1, replicas.size());
-      var badBroker = replicas.get(0).broker();
+      var badBroker = replicas.stream().map(Replica::broker).collect(Collectors.toSet());
+      var targetBroker =
+          topicAdmin.brokerIds().stream()
+              .filter(b -> !badBroker.contains(b))
+              .collect(Collectors.toSet());
       var argument = new ReplicaCollie.Argument();
-      argument.fromBrokers = Set.of(badBroker);
-      argument.toBrokers = Set.of();
+      argument.fromBrokers = badBroker;
+      argument.toBrokers = targetBroker;
       argument.brokers = bootstrapServers();
+      argument.topics = Set.of(topicName);
+      argument.partitions = Set.of(0);
       argument.verify = verify;
       var result = ReplicaCollie.execute(topicAdmin, argument);
       var assignment = result.get(new TopicPartition(topicName, 0));
-      Assertions.assertEquals(1, assignment.getKey().size());
-      Assertions.assertEquals(badBroker, assignment.getKey().iterator().next());
-      Assertions.assertEquals(1, assignment.getValue().size());
-      Assertions.assertFalse(assignment.getValue().contains(badBroker));
+      Assertions.assertEquals(badBroker, assignment.brokerSource);
+      Assertions.assertNotEquals(badBroker, assignment.brokerSink);
       if (verify) {
         var currentReplicas = topicAdmin.replicas(Set.of(topicName));
         Assertions.assertEquals(partitionReplicas.size(), currentReplicas.size());
@@ -67,11 +161,17 @@ public class ReplicaCollieTest extends RequireBrokerCluster {
               for (var index = 0; index != rs.size(); ++index)
                 Assertions.assertEquals(rs.get(index), currentRs.get(index));
             });
+        Assertions.assertEquals(targetBroker, assignment.brokerSink);
+
       } else {
         Utils.waitFor(
             () -> {
               var rs = topicAdmin.replicas(Set.of(topicName)).get(new TopicPartition(topicName, 0));
-              return rs.size() == 1 && rs.stream().noneMatch(r -> r.broker() == badBroker);
+              return rs.size() == 2
+                  && rs.stream()
+                      .map(Replica::broker)
+                      .collect(Collectors.toSet())
+                      .containsAll(argument.toBrokers);
             });
       }
     }
