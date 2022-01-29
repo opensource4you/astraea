aed97ee7b1a31760ce52dde4ceb9a2f144bafee8
wycccccc <493172422@qq.com>
2022-01-30 00:56:47 +0800

    spotless

--- app/src/main/java/org/astraea/dependency/DependencyRecord.java
+++ app/src/main/java/org/astraea/dependency/DependencyRecord.java
@@ -14,70 +14,73 @@ import org.apache.kafka.common.header.Header;
  * producer.send(dependencyRecord.orderRecord());
  * }</pre>
  */
-public class DependencyRecord<K,V> {
-    private final int partition;
-    public DependencyRecord() {
-        //TODO
-        this.partition = 0;
-    }
+public class DependencyRecord<K, V> {
+  private final int partition;
 
-    /**
-     * Creates a record with a specified timestamp to be sent to a specified topic and partition
-     *
-     * @param topic The topic the record will be appended to
-     * @param partition The partition to which the record should be sent
-     * @param timestamp The timestamp of the record, in milliseconds since epoch. If null, the producer will assign the
-     *                  timestamp using System.currentTimeMillis().
-     * @param key The key that will be included in the record
-     * @param value The record contents
-     */
-    public ProducerRecord<K,V> orderRecord(String topic, Integer partition, Long timestamp, K key, V value) {
-        return new ProducerRecord<>(topic, partition, timestamp, key, value, null);
-    }
+  public DependencyRecord() {
+    // TODO
+    this.partition = 0;
+  }
 
-    /**
-     * Creates a record to be sent to a specified topic and partition
-     *
-     * @param topic The topic the record will be appended to
-     * @param partition The partition to which the record should be sent
-     * @param key The key that will be included in the record
-     * @param value The record contents
-     * @param headers The headers that will be included in the record
-     */
-    public ProducerRecord<K,V> orderRecord(String topic, Integer partition, K key, V value, Iterable<Header> headers) {
-        return new ProducerRecord<>(topic, partition, null, key, value, headers);
-    }
+  /**
+   * Creates a record with a specified timestamp to be sent to a specified topic and partition
+   *
+   * @param topic The topic the record will be appended to
+   * @param partition The partition to which the record should be sent
+   * @param timestamp The timestamp of the record, in milliseconds since epoch. If null, the
+   *     producer will assign the timestamp using System.currentTimeMillis().
+   * @param key The key that will be included in the record
+   * @param value The record contents
+   */
+  public ProducerRecord<K, V> orderRecord(
+      String topic, Integer partition, Long timestamp, K key, V value) {
+    return new ProducerRecord<>(topic, partition, timestamp, key, value, null);
+  }
 
-    /**
-     * Creates a record to be sent to a specified topic and partition
-     *
-     * @param topic The topic the record will be appended to
-     * @param partition The partition to which the record should be sent
-     * @param key The key that will be included in the record
-     * @param value The record contents
-     */
-    public ProducerRecord<K,V> orderRecord(String topic, Integer partition, K key, V value) {
-        return new ProducerRecord<>(topic, partition, null, key, value, null);
-    }
+  /**
+   * Creates a record to be sent to a specified topic and partition
+   *
+   * @param topic The topic the record will be appended to
+   * @param partition The partition to which the record should be sent
+   * @param key The key that will be included in the record
+   * @param value The record contents
+   * @param headers The headers that will be included in the record
+   */
+  public ProducerRecord<K, V> orderRecord(
+      String topic, Integer partition, K key, V value, Iterable<Header> headers) {
+    return new ProducerRecord<>(topic, partition, null, key, value, headers);
+  }
 
-    /**
-     * Create a record to be sent to Kafka
-     *
-     * @param topic The topic the record will be appended to
-     * @param key The key that will be included in the record
-     * @param value The record contents
-     */
-    public ProducerRecord<K,V> orderRecord(String topic, K key, V value) {
-        return new ProducerRecord<>(topic, partition, null, key, value, null);
-    }
+  /**
+   * Creates a record to be sent to a specified topic and partition
+   *
+   * @param topic The topic the record will be appended to
+   * @param partition The partition to which the record should be sent
+   * @param key The key that will be included in the record
+   * @param value The record contents
+   */
+  public ProducerRecord<K, V> orderRecord(String topic, Integer partition, K key, V value) {
+    return new ProducerRecord<>(topic, partition, null, key, value, null);
+  }
 
-    /**
-     * Create a record with no key
-     *
-     * @param topic The topic this record should be sent to
-     * @param value The record contents
-     */
-    public ProducerRecord<K,V> orderRecord(String topic, V value) {
-        return new ProducerRecord<>(topic, partition, null, null, value, null);
-    }
+  /**
+   * Create a record to be sent to Kafka
+   *
+   * @param topic The topic the record will be appended to
+   * @param key The key that will be included in the record
+   * @param value The record contents
+   */
+  public ProducerRecord<K, V> orderRecord(String topic, K key, V value) {
+    return new ProducerRecord<>(topic, partition, null, key, value, null);
+  }
+
+  /**
+   * Create a record with no key
+   *
+   * @param topic The topic this record should be sent to
+   * @param value The record contents
+   */
+  public ProducerRecord<K, V> orderRecord(String topic, V value) {
+    return new ProducerRecord<>(topic, partition, null, null, value, null);
+  }
 }
