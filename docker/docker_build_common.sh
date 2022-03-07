14c071e63d62e808c867b7903ffb5f5ab7c74ee0
qoo332001 <sean0651101@gmail.com>
2022-03-07 18:06:47 +0800

    fix bug

--- docker/docker_build_common.sh
+++ docker/docker_build_common.sh
@@ -37,6 +37,7 @@ function buildImageIfNeed() {
       fi
     fi
     if [[ "$needToBuild" == "true" ]]; then
+      generateDockerfile
       docker build --no-cache -t "$imageName" -f "$DOCKERFILE" "$DOCKER_FOLDER"
       if [[ "$?" != "0" ]]; then
         exit 2
