6b1c2e8d7690397aef362aca2fc694ff9e244bba
Chia-Ping Tsai <chia7712@gmail.com>
2022-03-28 21:55:58 +0800

    Tweak transactional producer

--- app/src/main/java/org/astraea/producer/TransactionalSender.java
+++
@@ -1,7 +0,0 @@
-package org.astraea.producer;
-
-import java.util.concurrent.CompletionStage;
-
-public abstract class TransactionalSender<Key, Value> extends AbstractSender<Key, Value> {
-  abstract CompletionStage<Metadata> send();
-}
