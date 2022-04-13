9be01567706980d9f851847eff88e61bb44a6019
Zheng-Xian Li <garyparrottt@gmail.com>
2022-04-14 01:03:39 +0800

    revise data classes implementation

--- app/src/main/java/org/astraea/cost/PartitionInfo.java
+++ app/src/main/java/org/astraea/cost/PartitionInfo.java
@@ -1,9 +1,23 @@
 package org.astraea.cost;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public interface PartitionInfo {
 
   static PartitionInfo of(org.apache.kafka.common.PartitionInfo pf) {
-    return of(pf.topic(), pf.partition(), NodeInfo.of(pf.leader()));
+    return of(
+        pf.topic(),
+        pf.partition(),
+        NodeInfo.of(pf.leader()),
+        Arrays.stream(pf.replicas()).map(NodeInfo::of).collect(Collectors.toUnmodifiableList()),
+        Arrays.stream(pf.inSyncReplicas())
+            .map(NodeInfo::of)
+            .collect(Collectors.toUnmodifiableList()),
+        Arrays.stream(pf.offlineReplicas())
+            .map(NodeInfo::of)
+            .collect(Collectors.toUnmodifiableList()));
   }
 
   static PartitionInfo of(String topic, int partition, NodeInfo leader) {
@@ -22,6 +36,61 @@ public interface PartitionInfo {
       public NodeInfo leader() {
         return leader;
       }
+
+      @Override
+      public List<NodeInfo> replicas() {
+        return null;
+      }
+
+      @Override
+      public List<NodeInfo> inSyncReplica() {
+        return null;
+      }
+
+      @Override
+      public List<NodeInfo> offlineReplicas() {
+        return null;
+      }
+    };
+  }
+
+  static PartitionInfo of(
+      String topic,
+      int partition,
+      NodeInfo leader,
+      List<NodeInfo> replicas,
+      List<NodeInfo> inSyncReplicas,
+      List<NodeInfo> offlineReplicas) {
+    return new PartitionInfo() {
+      @Override
+      public String topic() {
+        return topic;
+      }
+
+      @Override
+      public int partition() {
+        return partition;
+      }
+
+      @Override
+      public NodeInfo leader() {
+        return leader;
+      }
+
+      @Override
+      public List<NodeInfo> replicas() {
+        return List.copyOf(replicas);
+      }
+
+      @Override
+      public List<NodeInfo> inSyncReplica() {
+        return List.copyOf(inSyncReplicas);
+      }
+
+      @Override
+      public List<NodeInfo> offlineReplicas() {
+        return List.copyOf(offlineReplicas);
+      }
     };
   }
 
@@ -33,4 +102,13 @@ public interface PartitionInfo {
 
   /** @return information of leader node */
   NodeInfo leader();
+
+  /** @return a list of replicas */
+  List<NodeInfo> replicas();
+
+  /** @return a list of in-sync replicas */
+  List<NodeInfo> inSyncReplica();
+
+  /** @return a list of offline replicas */
+  List<NodeInfo> offlineReplicas();
 }
