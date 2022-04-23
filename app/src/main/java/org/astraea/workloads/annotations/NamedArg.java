3e177ffe0b86b64bf908df48bbcac036b16809e1
Zheng-Xian Li <garyparrottt@gmail.com>
2022-04-24 02:36:47 +0800

    comment for NamedArg

--- app/src/main/java/org/astraea/workloads/annotations/NamedArg.java
+++ app/src/main/java/org/astraea/workloads/annotations/NamedArg.java
@@ -4,7 +4,18 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Parameter;
 
+/**
+ * This {@link NamedArg} is used to annotate the argument of Workload function. The workload
+ * simulation framework access the argument information(argument name & type) by Java Reflection.
+ * But the name of each argument was stripped away during the compilation process. If someone
+ * accesses the name of any argument by {@link Parameter#getName()}, this will return something like
+ * "arg0", which is totally meaningless. To overcome this limitation, one can add the `-parameters`
+ * flag during the class compilation to keep the name of argument in classfile. But this will
+ * increase the complexity of build tool. Instead, we use this annotation to mark the name of each
+ * argument. Also provides some human-friendly description if the author willing to.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface NamedArg {
