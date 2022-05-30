ced659bd47a6f528c5e2a4b7cd849b3e50fcb212
Chia-Ping Tsai <chia7712@gmail.com>
2022-05-31 00:59:41 +0800

    simply normalizer

--- app/src/main/java/org/astraea/cost/Normalizer.java
+++ app/src/main/java/org/astraea/cost/Normalizer.java
@@ -1,8 +1,11 @@
 package org.astraea.cost;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * used to normalize data into a range between [0, 1]
@@ -24,19 +27,33 @@ public interface Normalizer<K> {
   static <K> Normalizer<K> minMax(boolean positive) {
     var comparator = Comparator.comparing(Double::doubleValue);
     return values -> {
-      double max = values.values().stream().max(comparator).orElse(0.0);
-      double min = values.values().stream().min(comparator).orElse(0.0);
+      double max = values.stream().max(comparator).orElse(0.0);
+      double min = values.stream().min(comparator).orElse(0.0);
       // there is nothing to rescale, so we just all same values
       if (max == min)
-        return values.entrySet().stream()
-            .collect(Collectors.toMap(Map.Entry::getKey, ignored -> 1.0));
-      return values.entrySet().stream()
-          .collect(
-              Collectors.toMap(
-                  Map.Entry::getKey,
-                  entry ->
-                      (positive ? entry.getValue() - min : max - entry.getValue()) / (max - min)));
+        return IntStream.range(0, values.size())
+            .mapToObj(ignored -> 1.0)
+            .collect(Collectors.toUnmodifiableList());
+      return values.stream()
+          .map(value -> (positive ? value - min : max - value) / (max - min))
+          .collect(Collectors.toUnmodifiableList());
     };
+  }
+
+  /**
+   * rescales the values into a range of [0,1]. this is a helper method to use map collection
+   * directly.
+   *
+   * @param values origin data
+   * @return rescaled data
+   */
+  default Map<K, Double> normalize(Map<K, Double> values) {
+    var order = new ArrayList<>(values.entrySet());
+    var result =
+        normalize(order.stream().map(Map.Entry::getValue).collect(Collectors.toUnmodifiableList()));
+    return IntStream.range(0, result.size())
+        .boxed()
+        .collect(Collectors.toMap(i -> order.get(i).getKey(), result::get));
   }
 
   /**
@@ -45,5 +62,5 @@ public interface Normalizer<K> {
    * @param values origin data
    * @return rescaled data
    */
-  Map<K, Double> normalize(Map<K, Double> values);
+  List<Double> normalize(List<Double> values);
 }
