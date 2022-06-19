66f2c9f041b28b2146f7f6b519ab13c3a9cc9cd5
Zheng-Xian Li <garyparrottt@gmail.com>
2022-06-19 15:04:36 +0800

    add javadoc for `RebalanceAdminImpl#new`

--- app/src/main/java/org/astraea/app/balancer/executor/RebalanceAdminImpl.java
+++ app/src/main/java/org/astraea/app/balancer/executor/RebalanceAdminImpl.java
@@ -40,6 +40,14 @@ class RebalanceAdminImpl implements RebalanceAdmin {
   private final Admin admin;
   private final Supplier<Map<Integer, Collection<HasBeanObject>>> metricSource;
 
+  /**
+   * Construct a implementation of {@link RebalanceAdmin}
+   *
+   * @param topicFilter to determine which topics are permitted for balance operation
+   * @param admin the actual {@link Admin} implementation
+   * @param metricSource the supplier for new metrics, this supplier should return the metrics that
+   *     {@link RebalancePlanExecutor#fetcher()} is interested.
+   */
   public RebalanceAdminImpl(
       Predicate<String> topicFilter,
       Admin admin,
