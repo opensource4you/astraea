8aee537be50c9634824799d2905528e41aea8c67
chinghongfang <fjh7777@gmail.com>
2022-07-17 10:16:00 +0800

    Fix comment

--- app/src/main/java/org/astraea/app/producer/Builder.java
+++ app/src/main/java/org/astraea/app/producer/Builder.java
@@ -71,8 +71,7 @@ public class Builder<Key, Value> {
 
   public Builder<Key, Value> partitionClassName(String partitionClassName) {
     // Don't set partitioner to make sure DefaultPartitioner (Kafka version 3.2 and before) or
-    // BuiltInPartitioner
-    // (Kafka version 3.3) both works.
+    // BuiltInPartitioner (Kafka version 3.3) both works.
     if (partitionClassName == null || partitionClassName.isEmpty()) return this;
     return config(ProducerConfig.PARTITIONER_CLASS_CONFIG, partitionClassName);
   }
