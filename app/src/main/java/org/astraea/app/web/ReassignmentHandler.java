fad89acaeb4ca44349a79dcae9fefc122740c82d
Chia-Ping Tsai <chia7712@gmail.com>
2022-08-22 02:46:39 +0800

    Support to execute migration plans by Reassignment Web API

--- app/src/main/java/org/astraea/app/web/ReassignmentHandler.java
+++ app/src/main/java/org/astraea/app/web/ReassignmentHandler.java
@@ -24,6 +24,7 @@ import org.astraea.app.admin.Admin;
 import org.astraea.app.admin.TopicPartition;
 
 public class ReassignmentHandler implements Handler {
+  static final String PLANS_KEY = "plans";
   static final String TOPIC_KEY = "topic";
   static final String PARTITION_KEY = "partition";
   static final String FROM_KEY = "from";
@@ -36,23 +37,31 @@ public class ReassignmentHandler implements Handler {
   }
 
   @Override
-  public Response post(PostRequest request) {
-    // case 0: move replica to another folder
-    if (request.has(BROKER_KEY, TOPIC_KEY, PARTITION_KEY, TO_KEY)) {
-      admin
-          .migrator()
-          .partition(request.value(TOPIC_KEY), request.intValue(PARTITION_KEY))
-          .moveTo(Map.of(request.intValue(BROKER_KEY), request.value(TO_KEY)));
-      return Response.ACCEPT;
-    }
-    // case 1: move replica to another broker
-    if (request.has(TOPIC_KEY, PARTITION_KEY, TO_KEY)) {
-      admin
-          .migrator()
-          .partition(request.value(TOPIC_KEY), request.intValue(PARTITION_KEY))
-          .moveTo(request.intValues(TO_KEY));
-      return Response.ACCEPT;
-    }
+  public Response post(PostRequest requests) {
+    var rs =
+        requests.requests(PLANS_KEY).stream()
+            .map(
+                request -> {
+                  // case 0: move replica to another folder
+                  if (request.has(BROKER_KEY, TOPIC_KEY, PARTITION_KEY, TO_KEY)) {
+                    admin
+                        .migrator()
+                        .partition(request.value(TOPIC_KEY), request.intValue(PARTITION_KEY))
+                        .moveTo(Map.of(request.intValue(BROKER_KEY), request.value(TO_KEY)));
+                    return Response.ACCEPT;
+                  }
+                  // case 1: move replica to another broker
+                  if (request.has(TOPIC_KEY, PARTITION_KEY, TO_KEY)) {
+                    admin
+                        .migrator()
+                        .partition(request.value(TOPIC_KEY), request.intValue(PARTITION_KEY))
+                        .moveTo(request.intValues(TO_KEY));
+                    return Response.ACCEPT;
+                  }
+                  return Response.BAD_REQUEST;
+                })
+            .collect(Collectors.toUnmodifiableList());
+    if (!rs.isEmpty() && rs.stream().allMatch(r -> r == Response.ACCEPT)) return Response.ACCEPT;
     return Response.BAD_REQUEST;
   }
 
