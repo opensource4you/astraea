ba761f56167ccabc6f81e970bfe99d7d7766f144
Zheng-Xian Li <garyparrottt@gmail.com>
2022-09-13 11:01:17 +0800

    update comment

--- common/src/main/java/org/astraea/common/Utils.java
+++ common/src/main/java/org/astraea/common/Utils.java
@@ -84,11 +84,11 @@ public final class Utils {
 
   public static <T> T construct(Class<T> target, Configuration configuration) {
     try {
-      // case 0: create cost function by configuration
+      // case 0: create the class by the given configuration
       var constructor = target.getConstructor(Configuration.class);
       return packException(() -> constructor.newInstance(configuration));
     } catch (NoSuchMethodException e) {
-      // case 1: create cost function by empty constructor
+      // case 1: create the class by empty constructor
       return packException(() -> target.getConstructor().newInstance());
     }
   }
