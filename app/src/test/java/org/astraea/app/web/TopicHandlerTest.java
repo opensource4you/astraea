9da14c6a6e19a883091a44becea7f9222ba1c3f3
Chia-Ping Tsai <chia7712@gmail.com>
2022-09-15 01:37:24 +0800

    Fix flay testCreateTopicByProbability (#715)

--- app/src/test/java/org/astraea/app/web/TopicHandlerTest.java
+++ app/src/test/java/org/astraea/app/web/TopicHandlerTest.java
@@ -314,15 +314,19 @@ public class TopicHandlerTest extends RequireBrokerCluster {
           Channel.ofRequest(
               PostRequest.of(
                   String.format(
-                      "{\"topics\":[{\"name\":\"%s\", \"partitions\":18, \"probability\": 0.5}]}",
+                      "{\"topics\":[{\"name\":\"%s\", \"partitions\":30, \"probability\": 0.5}]}",
                       topicName)));
       var topics = handler.post(request);
       Assertions.assertEquals(1, topics.topics.size());
-      var info = topics.topics.stream().filter(t -> t.name.equals(topicName)).findFirst().get();
+      Utils.waitFor(
+          () ->
+              ((TopicHandler.TopicInfo) handler.get(Channel.ofTarget(topicName))).partitions.size()
+                  == 30);
       var groupByBroker =
-          info.partitions.stream()
-              .flatMap(p -> p.replicas.stream())
-              .collect(Collectors.groupingBy(r -> r.broker));
+          ((TopicHandler.TopicInfo) handler.get(Channel.ofTarget(topicName)))
+              .partitions.stream()
+                  .flatMap(p -> p.replicas.stream())
+                  .collect(Collectors.groupingBy(r -> r.broker));
       // those brokers should host different number of partitions
       Assertions.assertEquals(
           3, groupByBroker.values().stream().map(List::size).collect(Collectors.toSet()).size());
