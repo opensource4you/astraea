ab3fc273d71e79d8286eb7bbc337a1d4a9a55384
geordie <g1geordie@gmail.com>
2022-09-14 23:12:58 +0800

    spotless

--- common/src/main/java/org/astraea/common/metrics/client/consumer/HasConsumerMetrics.java
+++ common/src/main/java/org/astraea/common/metrics/client/consumer/HasConsumerMetrics.java
@@ -23,49 +23,180 @@ public interface HasConsumerMetrics extends HasBeanObject {
   default String clientId() {
     return beanObject().properties().get("client-id");
   }
-  default double reauthenticationLatencyAvg() {return (double) beanObject().attributes().get("reauthentication-latency-avg");}
-  default double ioTimeNsTotal() {return (double) beanObject().attributes().get("io-time-ns-total");}
-  default double successfulAuthenticationTotal() {return (double) beanObject().attributes().get("successful-authentication-total");}
-  default double ioWaittimeTotal() {return (double) beanObject().attributes().get("io-waittime-total");}
-  default double committedTimeNsTotal() {return (double) beanObject().attributes().get("committed-time-ns-total");}
-  default double reauthenticationLatencyMax() {return (double) beanObject().attributes().get("reauthentication-latency-max");}
-  default double successfulAuthenticationRate() {return (double) beanObject().attributes().get("successful-authentication-rate");}
-  default double commitSyncTimeNsTotal() {return (double) beanObject().attributes().get("commit-sync-time-ns-total");}
-  default double failedAuthenticationTotal() {return (double) beanObject().attributes().get("failed-authentication-total");}
-  default double timeBetweenPollAvg() {return (double) beanObject().attributes().get("time-between-poll-avg");}
-  default double connectionCount() {return (double) beanObject().attributes().get("connection-count");}
-  default double responseTotal() {return (double) beanObject().attributes().get("response-total");}
-  default double requestRate() {return (double) beanObject().attributes().get("request-rate");}
-  default double incomingByteRate() {return (double) beanObject().attributes().get("incoming-byte-rate");}
-  default double lastPollSecondsAgo() {return (double) beanObject().attributes().get("last-poll-seconds-ago");}
-  default double successfulAuthenticationNoReauthTotal() {return (double) beanObject().attributes().get("successful-authentication-no-reauth-total");}
-  default double timeBetweenPollMax() {return (double) beanObject().attributes().get("time-between-poll-max");}
-  default double failedReauthenticationTotal() {return (double) beanObject().attributes().get("failed-reauthentication-total");}
-  default double selectRate() {return (double) beanObject().attributes().get("select-rate");}
-  default double successfulReauthenticationTotal() {return (double) beanObject().attributes().get("successful-reauthentication-total");}
-  default double requestTotal() {return (double) beanObject().attributes().get("request-total");}
-  default double ioTimeNsAvg() {return (double) beanObject().attributes().get("io-time-ns-avg");}
-  default double iotimeTotal() {return (double) beanObject().attributes().get("iotime-total");}
-  default double ioWaitTimeNsTotal() {return (double) beanObject().attributes().get("io-wait-time-ns-total");}
-  default double ioWaitRatio() {return (double) beanObject().attributes().get("io-wait-ratio");}
-  default double networkIoRate() {return (double) beanObject().attributes().get("network-io-rate");}
-  default double connectionCreationRate() {return (double) beanObject().attributes().get("connection-creation-rate");}
-  default double requestSizeAvg() {return (double) beanObject().attributes().get("request-size-avg");}
-  default double ioRatio() {return (double) beanObject().attributes().get("io-ratio");}
-  default double responseRate() {return (double) beanObject().attributes().get("response-rate");}
-  default double successfulReauthenticationRate() {return (double) beanObject().attributes().get("successful-reauthentication-rate");}
-  default double selectTotal() {return (double) beanObject().attributes().get("select-total");}
-  default double outgoingByteTotal() {return (double) beanObject().attributes().get("outgoing-byte-total");}
-  default double pollIdleRatioAvg() {return (double) beanObject().attributes().get("poll-idle-ratio-avg");}
-  default double connectionCloseRate() {return (double) beanObject().attributes().get("connection-close-rate");}
-  default double incomingByteTotal() {return (double) beanObject().attributes().get("incoming-byte-total");}
-  default double requestSizeMax() {return (double) beanObject().attributes().get("request-size-max");}
-  default double failedAuthenticationRate() {return (double) beanObject().attributes().get("failed-authentication-rate");}
-  default double outgoingByteRate() {return (double) beanObject().attributes().get("outgoing-byte-rate");}
-  default double failedReauthenticationRate() {return (double) beanObject().attributes().get("failed-reauthentication-rate");}
-  default double ioWaitTimeNsAvg() {return (double) beanObject().attributes().get("io-wait-time-ns-avg");}
-  default double connectionCloseTotal() {return (double) beanObject().attributes().get("connection-close-total");}
-  default double connectionCreationTotal() {return (double) beanObject().attributes().get("connection-creation-total");}
-  default double networkIoTotal() {return (double) beanObject().attributes().get("network-io-total");}
-}
 
+  default double reauthenticationLatencyAvg() {
+    return (double) beanObject().attributes().get("reauthentication-latency-avg");
+  }
+
+  default double ioTimeNsTotal() {
+    return (double) beanObject().attributes().get("io-time-ns-total");
+  }
+
+  default double successfulAuthenticationTotal() {
+    return (double) beanObject().attributes().get("successful-authentication-total");
+  }
+
+  default double ioWaittimeTotal() {
+    return (double) beanObject().attributes().get("io-waittime-total");
+  }
+
+  default double committedTimeNsTotal() {
+    return (double) beanObject().attributes().get("committed-time-ns-total");
+  }
+
+  default double reauthenticationLatencyMax() {
+    return (double) beanObject().attributes().get("reauthentication-latency-max");
+  }
+
+  default double successfulAuthenticationRate() {
+    return (double) beanObject().attributes().get("successful-authentication-rate");
+  }
+
+  default double commitSyncTimeNsTotal() {
+    return (double) beanObject().attributes().get("commit-sync-time-ns-total");
+  }
+
+  default double failedAuthenticationTotal() {
+    return (double) beanObject().attributes().get("failed-authentication-total");
+  }
+
+  default double timeBetweenPollAvg() {
+    return (double) beanObject().attributes().get("time-between-poll-avg");
+  }
+
+  default double connectionCount() {
+    return (double) beanObject().attributes().get("connection-count");
+  }
+
+  default double responseTotal() {
+    return (double) beanObject().attributes().get("response-total");
+  }
+
+  default double requestRate() {
+    return (double) beanObject().attributes().get("request-rate");
+  }
+
+  default double incomingByteRate() {
+    return (double) beanObject().attributes().get("incoming-byte-rate");
+  }
+
+  default double lastPollSecondsAgo() {
+    return (double) beanObject().attributes().get("last-poll-seconds-ago");
+  }
+
+  default double successfulAuthenticationNoReauthTotal() {
+    return (double) beanObject().attributes().get("successful-authentication-no-reauth-total");
+  }
+
+  default double timeBetweenPollMax() {
+    return (double) beanObject().attributes().get("time-between-poll-max");
+  }
+
+  default double failedReauthenticationTotal() {
+    return (double) beanObject().attributes().get("failed-reauthentication-total");
+  }
+
+  default double selectRate() {
+    return (double) beanObject().attributes().get("select-rate");
+  }
+
+  default double successfulReauthenticationTotal() {
+    return (double) beanObject().attributes().get("successful-reauthentication-total");
+  }
+
+  default double requestTotal() {
+    return (double) beanObject().attributes().get("request-total");
+  }
+
+  default double ioTimeNsAvg() {
+    return (double) beanObject().attributes().get("io-time-ns-avg");
+  }
+
+  default double iotimeTotal() {
+    return (double) beanObject().attributes().get("iotime-total");
+  }
+
+  default double ioWaitTimeNsTotal() {
+    return (double) beanObject().attributes().get("io-wait-time-ns-total");
+  }
+
+  default double ioWaitRatio() {
+    return (double) beanObject().attributes().get("io-wait-ratio");
+  }
+
+  default double networkIoRate() {
+    return (double) beanObject().attributes().get("network-io-rate");
+  }
+
+  default double connectionCreationRate() {
+    return (double) beanObject().attributes().get("connection-creation-rate");
+  }
+
+  default double requestSizeAvg() {
+    return (double) beanObject().attributes().get("request-size-avg");
+  }
+
+  default double ioRatio() {
+    return (double) beanObject().attributes().get("io-ratio");
+  }
+
+  default double responseRate() {
+    return (double) beanObject().attributes().get("response-rate");
+  }
+
+  default double successfulReauthenticationRate() {
+    return (double) beanObject().attributes().get("successful-reauthentication-rate");
+  }
+
+  default double selectTotal() {
+    return (double) beanObject().attributes().get("select-total");
+  }
+
+  default double outgoingByteTotal() {
+    return (double) beanObject().attributes().get("outgoing-byte-total");
+  }
+
+  default double pollIdleRatioAvg() {
+    return (double) beanObject().attributes().get("poll-idle-ratio-avg");
+  }
+
+  default double connectionCloseRate() {
+    return (double) beanObject().attributes().get("connection-close-rate");
+  }
+
+  default double incomingByteTotal() {
+    return (double) beanObject().attributes().get("incoming-byte-total");
+  }
+
+  default double requestSizeMax() {
+    return (double) beanObject().attributes().get("request-size-max");
+  }
+
+  default double failedAuthenticationRate() {
+    return (double) beanObject().attributes().get("failed-authentication-rate");
+  }
+
+  default double outgoingByteRate() {
+    return (double) beanObject().attributes().get("outgoing-byte-rate");
+  }
+
+  default double failedReauthenticationRate() {
+    return (double) beanObject().attributes().get("failed-reauthentication-rate");
+  }
+
+  default double ioWaitTimeNsAvg() {
+    return (double) beanObject().attributes().get("io-wait-time-ns-avg");
+  }
+
+  default double connectionCloseTotal() {
+    return (double) beanObject().attributes().get("connection-close-total");
+  }
+
+  default double connectionCreationTotal() {
+    return (double) beanObject().attributes().get("connection-creation-total");
+  }
+
+  default double networkIoTotal() {
+    return (double) beanObject().attributes().get("network-io-total");
+  }
+}
