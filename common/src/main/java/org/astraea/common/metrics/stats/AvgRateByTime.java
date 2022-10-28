92b27eb881e7579c2c8a40ad551eabc7c7a69a13
chinghongfang <fjh7777@gmail.com>
2022-10-28 13:23:50 +0800

    Statistic on gauge

---
+++ common/src/main/java/org/astraea/common/metrics/stats/AvgRateByTime.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.astraea.common.metrics.stats;
+
+import java.time.Duration;
+
+public class AvgRateByTime implements Stat<Double> {
+  private Double accumulate = 0.0;
+
+  private long count = 0;
+
+  private final RangedDataCalculator<Double> rangedDataCalculator;
+
+  public AvgRateByTime(Duration period) {
+    this.rangedDataCalculator = new OnceByPeriod<>(period);
+  }
+
+  @Override
+  public synchronized void record(Double value) {
+    long current = System.currentTimeMillis();
+    rangedDataCalculator
+        .record(value, current)
+        .ifPresent(
+            rangedValue -> {
+              accumulate += rangedValue;
+              ++count;
+            });
+  }
+
+  @Override
+  public synchronized Double measure() {
+    return accumulate / count;
+  }
+}
