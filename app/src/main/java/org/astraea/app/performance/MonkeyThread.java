opensource4you/astraea

commit d539f4e032a016cd125b999e4b8e6191058f08b1

[PERF] Enhance performance pattern (#1049)
* Enhance performance tool to support pattern subscribe

app/src/main/java/org/astraea/app/performance/MonkeyThread.java
@@ -19,6 +19,7 @@ package org.astraea.app.performance;
 import java.time.Duration;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -84,7 +85,9 @@ public class MonkeyThread implements AbstractThread {
                   ConsumerThread.create(
                           1,
                           (clientId, listener) ->
-                              Consumer.forTopics(new HashSet<>(param.topics))
+                              (param.pattern == null
+                                      ? Consumer.forTopics(Set.copyOf(param.topics))
+                                      : Consumer.forTopics(param.pattern))
                                   .configs(param.configs())
                                   .config(
                                       ConsumerConfigs.ISOLATION_LEVEL_CONFIG,

app/src/main/java/org/astraea/app/performance/Performance.java
@@ -31,6 +31,7 @@ import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Supplier;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.astraea.common.DataRate;
 import org.astraea.common.DataSize;
@@ -45,6 +46,7 @@ import org.astraea.common.argument.DurationMapField;
 import org.astraea.common.argument.NonEmptyStringField;
 import org.astraea.common.argument.NonNegativeShortField;
 import org.astraea.common.argument.PathField;
+import org.astraea.common.argument.PatternField;
 import org.astraea.common.argument.PositiveIntegerField;
 import org.astraea.common.argument.PositiveIntegerListField;
 import org.astraea.common.argument.PositiveLongField;
@@ -92,7 +94,6 @@ public class Performance {
             param.producers,
             param::createProducer,
             param.interdependent);
-
     var consumerThreads =
         param.monkeys != null
             ? Collections.synchronizedList(new ArrayList<>(consumers(param, latestOffsets)))
@@ -148,7 +149,9 @@ public class Performance {
     return ConsumerThread.create(
         param.consumers,
         (clientId, listener) ->
-            Consumer.forTopics(new HashSet<>(param.topics))
+            (param.pattern == null
+                    ? Consumer.forTopics(Set.copyOf(param.topics))
+                    : Consumer.forTopics(param.pattern))
                 .configs(param.configs())
                 .config(
                     ConsumerConfigs.ISOLATION_LEVEL_CONFIG,
@@ -172,6 +175,12 @@ public class Performance {
         listConverter = StringListField.class,
         required = true)
     List<String> topics;
+
+    @Parameter(
+        names = {"--pattern"},
+        description = "Pattern: topic pattern(s) which consumers subscribed",
+        converter = PatternField.class)
+    Pattern pattern = null;
 
     void checkTopics() {
       try (var admin = Admin.of(configs())) {

common/src/main/java/org/astraea/common/argument/PatternField.java
@@ -1,0 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.astraea.common.argument;
+
+import java.util.regex.Pattern;
+
+public class PatternField extends Field<Pattern> {
+  @Override
+  public Pattern convert(String value) {
+    return Pattern.compile(value);
+  }
+}

common/src/main/java/org/astraea/common/consumer/Consumer.java
@@ -18,7 +18,9 @@ package org.astraea.common.consumer;
 
 import java.time.Duration;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
+import java.util.regex.Pattern;
 import org.astraea.common.admin.TopicPartition;
 
 /** An interface for polling records. */
@@ -75,6 +77,15 @@ public interface Consumer<Key, Value> extends AutoCloseable {
     return new TopicsBuilder<>(topics);
   }
 
+  /**
+   * Create a consumer builder by setting specific pattern.
+   *
+   * @param patternTopics patterns of topic name
+   * @return consumer builder for pattern topics
+   */
+  static TopicsBuilder<byte[], byte[]> forTopics(Pattern patternTopics) {
+    return new TopicsBuilder<>(Objects.requireNonNull(patternTopics));
+  }
   /**
    * Create a consumer builder by setting specific topic partitions
    *

common/src/main/java/org/astraea/common/consumer/TopicsBuilder.java
@@ -25,17 +25,25 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.astraea.common.Utils;
 import org.astraea.common.admin.TopicPartition;
 
 public class TopicsBuilder<Key, Value> extends Builder<Key, Value> {
-  private final Set<String> topics;
+  private final Set<String> setTopics;
+  private final Pattern patternTopics;
   private ConsumerRebalanceListener listener = ignore -> {};
 
   TopicsBuilder(Set<String> topics) {
-    this.topics = requireNonNull(topics);
+    this.patternTopics = null;
+    this.setTopics = requireNonNull(topics);
+  }
+
+  TopicsBuilder(Pattern patternTopics) {
+    this.setTopics = null;
+    this.patternTopics = patternTopics;
   }
 
   public TopicsBuilder<Key, Value> consumerRebalanceListener(ConsumerRebalanceListener listener) {
@@ -98,8 +106,15 @@ public class TopicsBuilder<Key, Value> extends Builder<Key, Value> {
     if (seekStrategy != SeekStrategy.NONE) {
       // make sure this consumer is assigned before seeking
       var latch = new CountDownLatch(1);
-      kafkaConsumer.subscribe(
-          topics, ConsumerRebalanceListener.of(List.of(listener, ignored -> latch.countDown())));
+      if (patternTopics == null)
+        kafkaConsumer.subscribe(
+            setTopics,
+            ConsumerRebalanceListener.of(List.of(listener, ignored -> latch.countDown())));
+      else
+        kafkaConsumer.subscribe(
+            patternTopics,
+            ConsumerRebalanceListener.of(List.of(listener, ignored -> latch.countDown())));
+
       while (latch.getCount() != 0) {
         // the offset will be reset, so it is fine to poll data
         // TODO: should we disable auto-commit here?
@@ -108,25 +123,30 @@ public class TopicsBuilder<Key, Value> extends Builder<Key, Value> {
       }
     } else {
       // nothing to seek so we just subscribe topics
-      kafkaConsumer.subscribe(topics, ConsumerRebalanceListener.of(List.of(listener)));
+      if (patternTopics == null)
+        kafkaConsumer.subscribe(setTopics, ConsumerRebalanceListener.of(List.of(listener)));
+      else kafkaConsumer.subscribe(patternTopics, ConsumerRebalanceListener.of(List.of(listener)));
     }
 
     seekStrategy.apply(kafkaConsumer, seekValue);
 
-    return new SubscribedConsumerImpl<>(kafkaConsumer, topics, listener);
+    return new SubscribedConsumerImpl<>(kafkaConsumer, setTopics, patternTopics, listener);
   }
 
   private static class SubscribedConsumerImpl<Key, Value> extends Builder.BaseConsumer<Key, Value>
       implements SubscribedConsumer<Key, Value> {
-    private final Set<String> topics;
+    private final Set<String> setTopics;
+    private final Pattern patternTopics;
     private final ConsumerRebalanceListener listener;
 
     public SubscribedConsumerImpl(
         org.apache.kafka.clients.consumer.Consumer<Key, Value> kafkaConsumer,
-        Set<String> topics,
+        Set<String> setTopics,
+        Pattern patternTopics,
         ConsumerRebalanceListener listener) {
       super(kafkaConsumer);
-      this.topics = topics;
+      this.setTopics = setTopics;
+      this.patternTopics = patternTopics;
       this.listener = listener;
     }
 
@@ -151,7 +171,9 @@ public class TopicsBuilder<Key, Value> extends Builder<Key, Value> {
 
     @Override
     protected void doResubscribe() {
-      kafkaConsumer.subscribe(topics, ConsumerRebalanceListener.of(List.of(listener)));
+      if (patternTopics == null)
+        kafkaConsumer.subscribe(setTopics, ConsumerRebalanceListener.of(List.of(listener)));
+      else kafkaConsumer.subscribe(patternTopics, ConsumerRebalanceListener.of(List.of(listener)));
     }
 
     @Override

common/src/test/java/org/astraea/common/argument/PatternFieldTest.java
@@ -1,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.astraea.common.argument;
+
+import com.beust.jcommander.Parameter;
+import java.util.regex.Pattern;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PatternFieldTest {
+  private static class FakeParameter {
+    @Parameter(
+        names = {"--field"},
+        converter = PatternField.class)
+    Pattern value;
+  }
+
+  @Test
+  public void testConvert() {
+    var param = Argument.parse(new FakeParameter(), new String[] {"--field", "test.*"});
+
+    Assertions.assertTrue(param.value.matcher("test").matches());
+    Assertions.assertFalse(param.value.matcher("tes").matches());
+  }
+}

commit a5bab329e9a6119d1cfded4e9e65c08e8216c932

Fix failed build caused by #1049

app/src/main/java/org/astraea/app/performance/MonkeyThread.java
@@ -17,7 +17,6 @@
 package org.astraea.app.performance;
 
 import java.time.Duration;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;

app/src/main/java/org/astraea/app/performance/Performance.java
@@ -23,7 +23,6 @@ import java.nio.file.Path;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
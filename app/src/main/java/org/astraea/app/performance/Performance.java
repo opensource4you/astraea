a5bab329e9a6119d1cfded4e9e65c08e8216c932
Chia-Ping Tsai <chia7712@gmail.com>
2022-10-31 22:05:06 +0800

    Fix failed build caused by #1049

--- app/src/main/java/org/astraea/app/performance/Performance.java
+++ app/src/main/java/org/astraea/app/performance/Performance.java
@@ -23,7 +23,6 @@ import java.nio.file.Path;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
