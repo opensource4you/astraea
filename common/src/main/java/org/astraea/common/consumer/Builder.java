43e5b72cc5655eba4932aa7bcf4e78520e1e46ed
Chia-Ping Tsai <chia7712@gmail.com>
2022-11-08 00:34:14 +0800

    Enable consumer builder to build stream directly (#1094)

--- common/src/main/java/org/astraea/common/consumer/Builder.java
+++ common/src/main/java/org/astraea/common/consumer/Builder.java
@@ -20,11 +20,22 @@ import static java.util.Objects.requireNonNull;
 
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 import org.astraea.common.Utils;
 import org.astraea.common.admin.TopicPartition;
 
@@ -109,6 +120,64 @@ public abstract class Builder<Key, Value> {
    */
   public abstract Consumer<Key, Value> build();
 
+  /**
+   * build a record stream by invoking a background running thread. The thread will close the
+   * consumer when closeFlag returns true;
+   *
+   * @param shouldClose to terminate the stream. The first element is the number of fetched records.
+   *     The second element is the elapsed time in ms.
+   * @return stream with records
+   */
+  public Stream<Record<Key, Value>> stream(BiFunction<Integer, Long, Boolean> shouldClose) {
+    var queue = new LinkedBlockingQueue<Optional<Record<Key, Value>>>();
+    var exception = new AtomicReference<RuntimeException>();
+    CompletableFuture.runAsync(
+        () -> {
+          var start = System.currentTimeMillis();
+          try (var consumer = build()) {
+            var count = 0;
+            while (!shouldClose.apply(count, System.currentTimeMillis() - start)) {
+              var records = consumer.poll(Duration.ofSeconds(2));
+              records.forEach(r -> queue.add(Optional.of(r)));
+              count += records.size();
+            }
+          } catch (RuntimeException e) {
+            exception.set(e);
+          } finally {
+            queue.add(Optional.empty());
+          }
+        });
+
+    return StreamSupport.stream(
+        Spliterators.spliteratorUnknownSize(
+            new Iterator<>() {
+
+              private Record<Key, Value> current;
+
+              @Override
+              public boolean hasNext() {
+                if (current != null) return true;
+                if (exception.get() != null) throw exception.get();
+                var record = Utils.packException(queue::take);
+                if (record.isEmpty()) return false;
+                current = record.get();
+                return true;
+              }
+
+              @Override
+              public Record<Key, Value> next() {
+                if (current == null) throw new NoSuchElementException("there is no more record");
+                try {
+                  return current;
+                } finally {
+                  current = null;
+                }
+              }
+            },
+            Spliterator.ORDERED),
+        false);
+  }
+
   protected abstract static class BaseConsumer<Key, Value> implements Consumer<Key, Value> {
     protected final org.apache.kafka.clients.consumer.Consumer<Key, Value> kafkaConsumer;
     private final AtomicBoolean subscribed = new AtomicBoolean(true);
@@ -122,7 +191,7 @@ public abstract class Builder<Key, Value> {
     }
 
     @Override
-    public Collection<Record<Key, Value>> poll(int recordCount, Duration timeout) {
+    public List<Record<Key, Value>> poll(int recordCount, Duration timeout) {
       var end = System.currentTimeMillis() + timeout.toMillis();
       var records = new ArrayList<Record<Key, Value>>();
       while (records.size() < recordCount) {
