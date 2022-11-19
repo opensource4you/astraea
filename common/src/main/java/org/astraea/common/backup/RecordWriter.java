5883c0d5fbfb178714a4b3ab375d264ffcf7408d
Chia-Ping Tsai <chia7712@gmail.com>
2022-11-19 22:52:23 +0800

    [BAKCUP] Apply RecordWriter on Exporter (#1138)

--- common/src/main/java/org/astraea/common/backup/RecordWriter.java
+++ common/src/main/java/org/astraea/common/backup/RecordWriter.java
@@ -17,88 +17,29 @@
 package org.astraea.common.backup;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.WritableByteChannel;
-import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
 import org.astraea.common.consumer.Record;
 
 public interface RecordWriter extends AutoCloseable {
+  void append(Record<byte[], byte[]> record);
 
-  static void write(File file, short version, Iterator<Record<byte[], byte[]>> records) {
-    switch (version) {
-      case 0:
-        try (var writer = new FileOutputStream(file)) {
-          var channel = writer.getChannel();
-          channel.write(ByteBufferUtils.of(version));
-          writeV0(channel, records);
-          writer.flush();
-        } catch (IOException e) {
-          throw new RuntimeException(e);
-        }
-        break;
-      default:
-        throw new IllegalArgumentException("unsupported version: " + version);
+  void flush();
+
+  @Override
+  void close();
+
+  static RecordWriterBuilder builder(File file) {
+    try {
+      return builder(new FileOutputStream(file));
+    } catch (FileNotFoundException e) {
+      throw new UncheckedIOException(e);
     }
   }
 
-  private static void writeV0(WritableByteChannel channel, Iterator<Record<byte[], byte[]>> records)
-      throws IOException {
-    var count = 0;
-    while (records.hasNext()) {
-      var record = records.next();
-      var topicBytes = record.topic().getBytes(StandardCharsets.UTF_8);
-      var recordSize =
-          2 // [topic size 2bytes]
-              + topicBytes.length // [topic]
-              + 4 // [partition 4bytes]
-              + 8 // [offset 8bytes]
-              + 8 // [timestamp 8bytes]
-              + 4 // [key length 4bytes]
-              + (record.key() == null ? 0 : record.key().length) // [key]
-              + 4 // [value length 4bytes]
-              + (record.value() == null ? 0 : record.value().length) // [value]
-              + 4 // [header size 4bytes]
-              + record.headers().stream()
-                  .mapToInt(
-                      h ->
-                          2 // [header key length 2bytes]
-                              + (h.key() == null
-                                  ? 0
-                                  : h.key().getBytes(StandardCharsets.UTF_8).length) // [header key]
-                              + 4 // [header value length 4bytes]
-                              + (h.value() == null ? 0 : h.value().length) // [header value]
-                      )
-                  .sum();
-      var recordBuffer = ByteBuffer.allocate(4 + recordSize);
-      recordBuffer.putInt(recordSize);
-      ByteBufferUtils.putLengthString(recordBuffer, record.topic());
-      recordBuffer.putInt(record.partition());
-      recordBuffer.putLong(record.offset());
-      recordBuffer.putLong(record.timestamp());
-      ByteBufferUtils.putLengthBytes(recordBuffer, record.key());
-      ByteBufferUtils.putLengthBytes(recordBuffer, record.value());
-      recordBuffer.putInt(record.headers().size());
-      record
-          .headers()
-          .forEach(
-              h -> {
-                ByteBufferUtils.putLengthString(recordBuffer, h.key());
-                ByteBufferUtils.putLengthBytes(recordBuffer, h.value());
-              });
-      channel.write(recordBuffer.flip());
-      count++;
-    }
-    channel.write(ByteBufferUtils.of(count));
-  }
-
-  void append(Record<byte[], byte[]> record) throws IOException;
-
-  void flush() throws IOException;
-
-  static RecordWriterBuilder builder() {
-    return new RecordWriterBuilder();
+  static RecordWriterBuilder builder(OutputStream outputStream) {
+    return new RecordWriterBuilder(RecordWriterBuilder.LATEST_VERSION, outputStream);
   }
 }
