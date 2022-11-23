ee85f074be03728faa5b380ac9b890ce92c4f64d
Zheng-Xian Li <garyparrottt@gmail.com>
2022-11-24 01:42:50 +0800

    update `Replica#toString`

--- common/src/main/java/org/astraea/common/admin/Replica.java
+++ common/src/main/java/org/astraea/common/admin/Replica.java
@@ -82,8 +82,8 @@ public interface Replica extends ReplicaInfo {
         + ", path='"
         + replica.path()
         + '\''
-        + ", leader="
-        + replica.isLeader()
+        + ", "
+        + (replica.isOffline() ? "offline" : replica.isLeader() ? "leader" : "follower")
         + '}';
   }
 }
