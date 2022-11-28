f3495a6ae9ee824ce30a03a41344a7f21f3215bf
chinghongfang <fjh7777@gmail.com>
2022-11-28 14:23:42 +0800

    Use replicaInfo directly

--- common/src/main/java/org/astraea/common/partitioner/smooth/SmoothWeightRoundRobinDispatcher.java
+++ common/src/main/java/org/astraea/common/partitioner/smooth/SmoothWeightRoundRobinDispatcher.java
@@ -18,13 +18,10 @@ package org.astraea.common.partitioner.smooth;
 
 import java.net.InetSocketAddress;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedDeque;
@@ -56,13 +53,6 @@ public class SmoothWeightRoundRobinDispatcher implements Dispatcher {
   private final Optional<Integer> jmxPortDefault = Optional.empty();
   private final Map<Integer, Integer> jmxPorts = new TreeMap<>();
 
-  /* These two map ("hasPartitions", "hasPartitionsSet") should be updated together. One for random access, one for existence checking*/
-  // Map of "broker id" and "collection of partitions"
-  // TODO: This is not aware of topic.
-  private final Map<Integer, List<Integer>> hasPartitions = new TreeMap<>();
-
-  private final Map<Integer, Set<Integer>> hasPartitionsSet = new TreeMap<>();
-
   private final Lazy<SmoothWeightRoundRobin> smoothWeightRoundRobinCal = Lazy.of();
 
   private final NeutralIntegratedCost neutralIntegratedCost = new NeutralIntegratedCost();
@@ -93,12 +83,14 @@ public class SmoothWeightRoundRobinDispatcher implements Dispatcher {
       var smooth = smoothWeightRoundRobinCal.get(() -> new SmoothWeightRoundRobin(supplier.get()));
       smooth.init(supplier);
       var targetBroker = smooth.getAndChoose(topic, clusterInfo);
+      var targetPartitions =
+          clusterInfo.availableReplicas(topic).stream()
+              .filter(r -> r.nodeInfo().id() == targetBroker)
+              .collect(Collectors.toUnmodifiableList());
       targetPartition =
-          hasPartitions
-              .get(targetBroker)
-              .get(
-                  nextValue(topic, clusterInfo, targetBroker)
-                      % hasPartitions.get(targetBroker).size());
+          targetPartitions
+              .get(nextValue(topic, clusterInfo, targetBroker) % targetPartitions.size())
+              .partition();
     }
 
     return targetPartition;
@@ -152,19 +144,6 @@ public class SmoothWeightRoundRobinDispatcher implements Dispatcher {
 
   private void refreshPartitionMetaData(ClusterInfo<ReplicaInfo> clusterInfo, String topic) {
     partitions = clusterInfo.availableReplicas(topic);
-
-    partitions.forEach(
-        p -> {
-          // Check for partition existence. If not (a new partition), add to candidate list
-          if (hasPartitionsSet
-              .computeIfAbsent(p.nodeInfo().id(), ignore -> new HashSet<>())
-              .add(p.partition())) {
-            hasPartitions
-                .computeIfAbsent(p.nodeInfo().id(), ignore -> new ArrayList<>())
-                .add(p.partition());
-          }
-        });
-
     neutralIntegratedCost
         .fetcher()
         .ifPresent(
