f2588ac78b21031289053a0cd4d9d7d40c026fd8
Chia-Ping Tsai <chia7712@gmail.com>
2022-12-11 00:29:24 +0800

    Move MBeanRegister from org.astraea.common.metrics.jmx to org.astraea… (#1248)

--- common/src/main/java/org/astraea/common/metrics/MBeanRegister.java
+++ common/src/main/java/org/astraea/common/metrics/MBeanRegister.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.astraea.common.metrics.jmx;
+package org.astraea.common.metrics;
 
 import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
