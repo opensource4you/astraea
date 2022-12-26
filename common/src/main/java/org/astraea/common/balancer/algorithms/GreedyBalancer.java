85f0a48b3ca38c8013da80e74ff2e3db82f2f640
Zheng-Xian Li <garyparrottt@gmail.com>
2022-12-26 23:51:26 +0800

    A a few considerations when tuning the max step number

--- common/src/main/java/org/astraea/common/balancer/algorithms/GreedyBalancer.java
+++ common/src/main/java/org/astraea/common/balancer/algorithms/GreedyBalancer.java
@@ -41,7 +41,41 @@ import org.astraea.common.metrics.MBeanRegister;
 public class GreedyBalancer implements Balancer {
 
   public static final String SHUFFLE_TWEAKER_MIN_STEP_CONFIG = "shuffle.tweaker.min.step";
+
+  /**
+   * The maximum alteration to a ClusterInfo in one cluster state exploration trial.
+   *
+   * <p>GreedyBalancer tries to discover a better way to allocate logs, by altering the given
+   * current log allocation. This config controls the maximum number of alteration in one go. The
+   * larger the number the bolder move it can be made in one go. But it doesn't mean the larger this
+   * config the better. On The Contrary, less move usually helps approach good allocation. There are
+   * a few things to consider when tuning this number:
+   *
+   * <ol>
+   *   <li>The mindset behind greedy balancer is discovering better allocation by making many steps
+   *       of move. Not making large move in one go.
+   *   <li>Larger move usually results in a terrible cluster state. Although this depends on the
+   *       implementation of the allocation tweaker, the allocation tweaker we are using now making
+   *       move blinding in the state space. So it might not propose a good move, especially when
+   *       more moves have been made the chance of getting a terrible state is higher. Due to this
+   *       reason making more moves might make it hard for GreedyBalancer to discover a good plan.
+   *   <li>Making many alterations in one go can helps to escape the local minimum. But in the
+   *       allocation problem we are facing right now, it is probably hard to get stuck in a bad
+   *       local minimum. (Warning, the following thought is totally based on my heuristic and a few
+   *       discussion on the Internet. I didn't prove this formally) The local minimum we see one
+   *       book is usually depict in two or three dimension space. With such low number of
+   *       dimension, it is hard to escape a local minimum since we have fewer dimension to choice
+   *       to. But when the dimension getting higher, there is more chance a state can slip out to
+   *       somewhere better since there is more choice to made. So it is hard to get stuck in a bad
+   *       local minimum when the dimension is higher. We probably don't need to worry about
+   *       escaping local minimum when solving the Kafka log allocation problem. The Kafka partition
+   *       allocation problem has a feature, it usually have a very high dimension. By dimension I
+   *       mean the thing can be tune, for each partition you can consider it as a dimension. So if
+   *       a cluster has 5000 partitions, there are 5000 knobs (dimension) that can be tune.
+   * </ol>
+   */
   public static final String SHUFFLE_TWEAKER_MAX_STEP_CONFIG = "shuffle.tweaker.max.step";
+
   public static final String ITERATION_CONFIG = "iteration";
   public static final Set<String> ALL_CONFIGS =
       new TreeSet<>(Utils.constants(GreedyBalancer.class, name -> name.endsWith("CONFIG")));
