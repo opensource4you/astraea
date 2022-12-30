8921814a20beb600c1e3b6c55c406a87e27b26a6
chinghongfang <fjh7777@gmail.com>
2022-12-30 15:09:45 +0800

    Remove self-testing test

--- common/src/test/java/org/astraea/common/metrics/collector/FromInternalTopicTest.java
+++ common/src/test/java/org/astraea/common/metrics/collector/FromInternalTopicTest.java
@@ -16,42 +16,16 @@
  */
 package org.astraea.common.metrics.collector;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import org.astraea.common.metrics.BeanObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FromInternalTopicTest {
   @Test
-  void testClusterBean() {
-    try (var collector = new FromInternalTopic("192.168.103.26:9092")) {
-      for (int i = 0; i < 10; ++i) {
-        Thread.sleep(5000);
-        getLogEndOffset(collector).forEach(System.out::println);
-        System.out.println("=====");
-      }
-    } catch (InterruptedException ie) {
-      ie.printStackTrace();
-    }
-  }
-
-  @Test
   void testStringToBean() {
     var bean =
         new BeanObject("domain", Map.of("name", "name1", "type", "type1"), Map.of("value", "v1"));
     Assertions.assertEquals(bean, FromInternalTopic.stringToBean(bean.toString()).beanObject());
-  }
-
-  private List<String> getLogEndOffset(MetricCollector collector) {
-    return collector.clusterBean().all().values().stream()
-        .flatMap(Collection::stream)
-        .filter(bean -> bean.beanObject().domainName().equals("kafka.log"))
-        .filter(bean -> bean.beanObject().properties().getOrDefault("name", "").equals("Size"))
-        .filter(bean -> bean.beanObject().properties().getOrDefault("topic", "").equals("simple"))
-        .map(hbo -> hbo.beanObject().toString())
-        .collect(Collectors.toUnmodifiableList());
   }
 }
