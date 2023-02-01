8216e066513564e2e01ced1d6b7138c23effe117
Zheng-Xian Li <garyparrottt@gmail.com>
2023-02-01 14:05:42 +0800

    update `BalancerConsole` draft

---
+++ common/src/main/java/org/astraea/common/balancer/BalancerConsole.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.astraea.common.balancer;
+
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
+import org.astraea.common.admin.Admin;
+import org.astraea.common.balancer.algorithms.AlgorithmConfig;
+import org.astraea.common.balancer.executor.RebalancePlanExecutor;
+
+/** Offer a uniform interface to schedule/manage/execute balance plan to an actual Kafka cluster. */
+public interface BalancerConsole extends AutoCloseable {
+
+  static BalancerConsole create(Admin admin, Function<Integer, Optional<Integer>> jmxPortMapper) {
+    throw new UnsupportedOperationException();
+  }
+
+  Collection<BalanceTask> tasks();
+
+  BalanceTask task(String taskId);
+
+  Generation launchRebalancePlanGeneration();
+
+  Execution launchRebalancePlanExecution();
+
+  @Override
+  void close();
+
+  interface Generation {
+
+    Generation setBalancer(Balancer balancer);
+
+    Generation setBalancer(Class<? extends Balancer> balancer);
+
+    Generation setBalancerConfig(AlgorithmConfig algorithmConfig);
+
+    Generation setGenerationTimeout(Duration timeout);
+
+    BalanceTask generate();
+  }
+
+  interface Execution {
+
+    Execution setExecutor(RebalancePlanExecutor executor);
+
+    Execution setExecutor(Class<? extends RebalancePlanExecutor> executor);
+
+    Execution setExecutionTimeout(Duration timeout);
+
+    default BalanceTask execute(BalanceTask theTask) {
+      return execute(theTask.taskId());
+    }
+
+    BalanceTask execute(String taskId);
+  }
+
+  interface BalanceTask {
+
+    String taskId();
+
+    Phase phase();
+
+    Balancer usedBalancer();
+
+    AlgorithmConfig usedBalancerConfig();
+
+    RebalancePlanExecutor usedExecutor();
+
+    CompletionStage<Balancer.Plan> planGeneration();
+
+    CompletionStage<Void> planExecution();
+
+    enum Phase {
+      Searching,
+      Searched,
+      Executing,
+      Executed
+    }
+  }
+}
