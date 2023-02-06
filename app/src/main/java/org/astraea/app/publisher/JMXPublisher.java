3a5c3df208d08fccf87df4a89a969c6c10eaa0f1
chinghongfang <fjh7777@gmail.com>
2023-02-06 18:56:54 +0800

    Metric publisher

---
+++ app/src/main/java/org/astraea/app/publisher/JMXPublisher.java
@@ -0,0 +1,136 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.astraea.app.publisher;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.astraea.app.performance.AbstractThread;
+import org.astraea.common.Utils;
+import org.astraea.common.admin.Admin;
+import org.astraea.common.admin.NodeInfo;
+import org.astraea.common.metrics.BeanQuery;
+import org.astraea.common.metrics.MBeanClient;
+import org.astraea.common.producer.Producer;
+import org.astraea.common.producer.Record;
+import org.astraea.common.producer.Serializer;
+
+public class JMXPublisher implements AbstractThread {
+  public static JMXPublisher create(
+      String bootstrapServer, Function<Integer, Integer> idToJmxPort, Duration period) {
+    var publisher = new JMXPublisher(bootstrapServer, idToJmxPort);
+    publisher.runPeriodic(period);
+    return publisher;
+  }
+
+  // Broker id and mbean client
+  private final Map<Integer, MBeanClient> idMBeanClient = new ConcurrentHashMap<>();
+
+  private final Admin admin;
+  private final Producer<byte[], String> producer;
+
+  private final Function<Integer, Integer> idToJmxPort;
+
+  private ScheduledFuture<?> future = null;
+
+  public JMXPublisher(String bootstrapServer, Function<Integer, Integer> idToJmxPort) {
+    this.admin = Admin.of(bootstrapServer);
+    this.producer =
+        Producer.builder()
+            .bootstrapServers(bootstrapServer)
+            .valueSerializer(Serializer.STRING)
+            .build();
+    this.idToJmxPort = idToJmxPort;
+  }
+
+  /**
+   * Periodically fetch jmx mbeans and publish to topics.
+   *
+   * @param period the period to fetch and publish mbeans
+   */
+  public void runPeriodic(Duration period) {
+    this.future =
+        Executors.newScheduledThreadPool(1)
+            .scheduleAtFixedRate(this::execute, 0, period.toMillis(), TimeUnit.MILLISECONDS);
+  }
+
+  // Fetch from mbeanClient and send to inner topic
+
+  public void execute() {
+    updateNodeInfo();
+    // Synchronously fetch metrics from jmx and produce to internal-topic
+    idMBeanClient.forEach(
+        (id, client) ->
+            CompletableFuture.supplyAsync(() -> client.beans(BeanQuery.all()))
+                .thenAccept(
+                    beans ->
+                        beans.stream()
+                            .map(
+                                bean ->
+                                    Record.builder()
+                                        .topic(MetricPublisher.internalTopicName(id))
+                                        .key((byte[]) null)
+                                        .value(bean.toString())
+                                        .build())
+                            .forEach(producer::send)));
+  }
+
+  public void updateNodeInfo() {
+    admin
+        .nodeInfos()
+        .thenAccept(
+            infos -> {
+              // Create jmx client when new node detected
+              infos.forEach(
+                  node ->
+                      this.idMBeanClient.computeIfAbsent(node.id(), id -> createMBeanClient(node)));
+              // Delete non-exist node and close those MBeanClients
+              var idSet = infos.stream().map(NodeInfo::id).collect(Collectors.toUnmodifiableSet());
+              this.idMBeanClient.keySet().stream()
+                  .filter(id -> !idSet.contains(id))
+                  .forEach(id -> this.idMBeanClient.remove(id).close());
+            });
+  }
+
+  @Override
+  public void waitForDone() {
+    Utils.swallowException(this.future::get);
+  }
+
+  @Override
+  public boolean closed() {
+    return future.isDone();
+  }
+
+  public void close() {
+    future.cancel(false);
+    waitForDone();
+    idMBeanClient.forEach((id, client) -> client.close());
+    admin.close();
+  }
+
+  private MBeanClient createMBeanClient(NodeInfo nodeInfo) {
+    var jmxPort = idToJmxPort.apply(nodeInfo.id());
+    return MBeanClient.jndi(nodeInfo.host(), jmxPort);
+  }
+}
