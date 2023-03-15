aae1ef05bcd5722344e661688b50949606c8663b
Zhi-Mao Teng <zhimao.teng@gmail.com>
2023-03-15 16:29:44 +0800

    Add masked for ClusterBean

--- common/src/main/java/org/astraea/common/partitioner/SmoothWeightRoundRobinPartitioner.java
+++ common/src/main/java/org/astraea/common/partitioner/SmoothWeightRoundRobinPartitioner.java
@@ -27,6 +27,7 @@ import java.util.stream.Collectors;
 import org.astraea.common.Configuration;
 import org.astraea.common.Utils;
 import org.astraea.common.admin.BrokerTopic;
+import org.astraea.common.admin.ClusterBean;
 import org.astraea.common.admin.ClusterInfo;
 import org.astraea.common.cost.NeutralIntegratedCost;
 import org.astraea.common.metrics.collector.LocalMetricCollector;
@@ -63,7 +64,11 @@ public class SmoothWeightRoundRobinPartitioner extends Partitioner {
     Supplier<Map<Integer, Double>> supplier =
         () ->
             // fetch the latest beans for each node
-            neutralIntegratedCost.brokerCost(clusterInfo, metricCollector.clusterBean()).value();
+            neutralIntegratedCost
+                .brokerCost(
+                    clusterInfo,
+                    ClusterBean.masked(metricCollector.clusterBean(), node -> node != -1))
+                .value();
 
     smoothWeightCal.refresh(supplier);
 
