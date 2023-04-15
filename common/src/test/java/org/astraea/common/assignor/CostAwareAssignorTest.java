4d4f0b4a12d33ed5104806011a488be465d21afb
harryteng9527 <zhimao.teng@gmail.com>
2023-04-15 21:26:04 +0800

    Add test for greedyAssign

--- common/src/test/java/org/astraea/common/assignor/CostAwareAssignorTest.java
+++ common/src/test/java/org/astraea/common/assignor/CostAwareAssignorTest.java
@@ -16,4 +16,76 @@
  */
 package org.astraea.common.assignor;
 
-public class CostAwareAssignorTest {}
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import org.astraea.common.admin.TopicPartition;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CostAwareAssignorTest {
+
+  @Test
+  void testGreedyAssign() {
+    var assignor = new CostAwareAssignor();
+    var subscription =
+        Map.of(
+            "c1",
+            new Subscription(List.of("t1", "t2", "t3"), null),
+            "c2",
+            new Subscription(List.of("t1", "t2", "t3"), null));
+    var cost1 =
+        Map.of(
+            TopicPartition.of("t1", 0),
+            0.2,
+            TopicPartition.of("t1", 1),
+            0.4,
+            TopicPartition.of("t2", 0),
+            0.2,
+            TopicPartition.of("t3", 0),
+            0.4);
+
+    var allSuitableAssignment = assignor.greedyAssign(subscription, cost1, Map.of());
+    Assertions.assertEquals(2, allSuitableAssignment.size());
+    allSuitableAssignment.forEach((c, assign) -> Assertions.assertEquals(2, assign.size()));
+
+    var incompatibility =
+        Map.of(
+            TopicPartition.of("t1", 0),
+            Set.of(TopicPartition.of("t3", 0)),
+            TopicPartition.of("t1", 1),
+            Set.of(TopicPartition.of("t2", 0)),
+            TopicPartition.of("t2", 0),
+            Set.of(TopicPartition.of("t1", 1)),
+            TopicPartition.of("t3", 0),
+            Set.of(TopicPartition.of("t1", 0)));
+    var incompatibleAssignment = assignor.greedyAssign(subscription, cost1, incompatibility);
+    Assertions.assertEquals(2, incompatibleAssignment.size());
+    incompatibleAssignment.forEach(
+        (consumer, assignment) -> {
+          assignment.forEach(
+              tp -> {
+                incompatibility
+                    .get(tp)
+                    .forEach(itp -> Assertions.assertFalse(assignment.contains(itp)));
+              });
+        });
+
+    Function<String, TopicPartition> create = TopicPartition::of;
+    var allIncompatible =
+        Map.of(
+            create.apply("t1-0"),
+            Set.of(create.apply("t1-1"), create.apply("t2-0"), create.apply("t3-0")),
+            create.apply("t1-1"),
+            Set.of(create.apply("t1-0"), create.apply("t2-0"), create.apply("t3-0")),
+            create.apply("t2-0"),
+            Set.of(create.apply("t1-0"), create.apply("t1-1"), create.apply("t3-0")),
+            create.apply("t3-0"),
+            Set.of(create.apply("t1-0"), create.apply("t2-0"), create.apply("t1-1")));
+
+    var allIncompatibleAssignment = assignor.greedyAssign(subscription, cost1, allIncompatible);
+    allIncompatibleAssignment.forEach(
+        (c, assignment) -> Assertions.assertEquals(2, assignment.size()));
+  }
+}
