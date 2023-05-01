186d57c07379dd7214711c4c8fd7adbba3bc86e0
Zheng-Xian Li <garyparrottt@gmail.com>
2023-05-01 22:34:31 +0800

    Revise Javadoc

--- common/src/main/java/org/astraea/common/cost/ResourceUsageHint.java
+++ common/src/main/java/org/astraea/common/cost/ResourceUsageHint.java
@@ -20,24 +20,24 @@ import java.util.function.Predicate;
 import org.astraea.common.admin.Replica;
 
 /**
- * ResourceUsageHint is an interface that is used to determine the resource usage of a cluster and a
- * replica for a specific balancing goal. The implementation of this class will be capable of
- * providing heuristics for the balancing goal, for example:
+ * ResourceUsageHint is an interface used to determine the resource usage of a cluster and a replica
+ * for a specific balancing goal. Implementations of this class provide heuristics for the balancing
+ * goal, such as:
  *
  * <ol>
- *   <li>the concrete resource usage of a given replica in terms of the current balancing problem.
- *   <li>the cluster resource usage of a given replica in terms of the current balancing problem.
- *   <li>the importance of a replica in terms of the current balancing problem.
- *   <li>the idealness of a given cluster resource usage in terms of a specific balancing goal
- *   <li>the validity of a cluster resource usage in terms of a specific balancing goal(probably a
+ *   <li>The concrete resource usage of a given replica in terms of the current balancing problem.
+ *   <li>The cluster resource usage of a given replica in terms of the current balancing problem.
+ *   <li>The importance of a replica in terms of the current balancing problem.
+ *   <li>The idealness of a given cluster resource usage in terms of a specific balancing goal
+ *   <li>The validity of a cluster resource usage in terms of a specific balancing goal(possibly a
  *       constraint)
  * </ol>
  *
- * This valuable information can be used to guide the solution search algorithm. Provide the
- * capability to discover solution spaces with higher potential to have good solutions. Or branch
- * out infeasible solutions earlier. The implementation of all these functions requires following
- * some important rules. Violating these rules might cause the algorithm to function poorly, or
- * unable to apply some optimization to speed up the search process.
+ * This valuable information can guide the solution search algorithm. Enabling it to discover
+ * solution spaces with higher potential for good solutions or branch out infeasible solutions
+ * earlier. Implementations of all these functions require following some important rules. Violating
+ * these rules might cause the algorithm to function poorly or unable to apply some optimizations to
+ * speed up the search process.
  */
 public interface ResourceUsageHint {
 
@@ -51,11 +51,11 @@ public interface ResourceUsageHint {
    * Determine the resource usage of a replica. Unlike {@link
    * ResourceUsageHint#evaluateClusterResourceUsage(Replica)}, the resource usage is unrelated to
    * the broker it is located at. The resource usage is somewhat fundamental to the replica itself.
-   * Not incurred during the placement decision been made.
    *
-   * <p>The return value is related to {@link ResourceUsageHint#importance(ResourceUsage)}. A
-   * replica with more resource consumption, its location within the cluster has more impact than
-   * the one with less resource consumption
+   * <p>The return value should be used in conjunction with {@link
+   * ResourceUsageHint#importance(ResourceUsage)}. A replica with more resource consumption, its
+   * location within the cluster will be more impactful than the one with less resource consumption.
+   * Which generally results in a higher importance score.
    *
    * @return the resource usage of a replica.
    */
@@ -64,51 +64,59 @@ public interface ResourceUsageHint {
   /**
    * Determine the resource usage incurred to a cluster when the replica is placed on its broker.
    *
-   * <p>The implementation should be stateless. That is given the sample replica input. It should
-   * always return the exactly same ResourceUsage content.
+   * <p>The implementation should be stateless, meaning that given the sample input, it should
+   * always return the same resource usage.
    *
    * @return the cluster resource usage incurred by this replica.
    */
   ResourceUsage evaluateClusterResourceUsage(Replica target);
 
   /**
-   * Determine the importance of the resource usage incurred by a replica. The return value must be
-   * within the range[0, 1]. Where 0 represents the replica will incur the highest resource
-   * consumption for a certain resource among other replicas. And 1 represents the replica will
-   * incur the lowest resource consumption for a certain resource among other replicas.
+   * Determine the importance of the resource usage that will be incurred a replica. The return
+   * value must be within the range [0, 1]. Where 0 represents the replica associated with the given
+   * resource usage will incur the highest resource consumption for a certain resource among other
+   * replicas. And 1 represents the replica will incur the lowest resource consumption for a certain
+   * resource among other replicas.
    */
   double importance(ResourceUsage replicaResourceUsage);
 
   /**
-   * The idealness of a cluster resource usage state. The return value must be within the range[0,
-   * 1]. Where 0 represents the most ideal usage of a certain resource(compared to the most ideal
-   * final cluster). And 1 represents the most undesired usage of a certain resource(compared to the
-   * most ideal final cluster).
+   * Compute the idealness score of the given cluster resource usage based on the balancing goal of
+   * this {@link ResourceUsageHint}. The return value must be within the range [0, 1]. Where 0
+   * represents the ideal usage of a certain resource(compared to the ideal complete cluster
+   * resource usage). And given two idealness scores of two cluster resource usage, <code>a</code>
+   * and <code>b</code>. If <code>a < b</code>. This means the cluster resource allocation of <code>
+   * a
+   * </code> is considered more ideal than the one with <code>b</code>, in terms of the specific
+   * balancing goal.
    *
-   * <p>The most ideal final cluster is a cluster that might only exist in imagination. It has all
-   * the replicas placed to brokers. And the resource usage after all the placements is considered
-   * in the most ideal state. <b>The idealness should be compared to the final cluster.</b> For
-   * example, if we have 100 replicas, and we want to distribute it to 5 brokers evenly. In this
-   * case, the most ideal final cluster we can imagine is the one that each broker has 20 replicas.
-   * A correct implementation can be measuring the distance between the given cluster resource
-   * usage([a,b,c,d,e]) to the most ideal usage([20,20,20,20,20]). And an inappropriate
-   * implementation might be not compared to the ideal cluster. But instead, calculate the average
-   * between the given resource usages avg=(a+b+c+d+e)/5. And measuring the distance between
-   * [a,b,c,d,e] to [avg,avg,avg,avg,avg]. The inappropriate implementation thinks the given cluster
-   * resource usage is the one where all the resources are set. But this {@link ResourceUsageHint}
-   * framework doesn't enforce this.
+   * <p>The term <b>complete cluster resource usage</b> refers to a cluster resource usage where all
+   * the replicas resource usage is covered. And the definition of a <b>ideal complete cluster
+   * resource usage</b> is where the complete cluster resource usage is considered ideal in terms of
+   * the balancing goal of this {@link ResourceUsageHint}.
+   *
+   * <p>For example, Suppose the {@link ResourceUsageHint} is about balancing the number of replicas
+   * between brokers. Then given 100 replicas and 5 brokers. The ideal resource placement will be
+   * {20, 20, 20, 20, 20}. In which the replicas are spread evenly. An example of an appropriate
+   * implementation can calculate the distance between the current placement {a,b,c,d,e} to
+   * {20,20,20,20,20}. If all value equals to 20, The idealness will be 0, which means the current
+   * resource usage is considered ideal. On the other hand, an <b>inappropriate</b> implementation
+   * might be measuring the distance between {a,b,c,d,e} to {avg,avg,avg,avg,avg} where avg is
+   * (a+b+c+d+e)/5. The problem with this approach is, the sum of the input resource usage might not
+   * be 100. The {@link ResourceUsageHint} framework doesn't require input resource usage be
+   * <b>complete</b>(to contain all the replicas).
    */
   double idealness(ResourceUsage clusterResourceUsage);
 
   /**
    * A {@link Predicate} to determine the validity of a cluster resource usage. Noted that this
-   * Predicate can only describe the exceeded usage of resources. It can describe constraints like:
-   * each broker can only have at most 30 leaders. But it can't describe logic like: each broker
-   * must have at least 30 leaders. With the requirement we can conclude a general law for the
+   * Predicate can only describe on exceeded usage of resources. Such as limiting the number of
+   * leader replicas a broker can have. It cannot describe logic such as requiring a minimum number
+   * of leaders per broker must have. With the requirement we can conclude a general law for the
    * return predicate, given an empty {@link ResourceUsage}, it should always return true.
    *
-   * <p>It is recommended to use {@link ResourceUsageHint#idealness(ResourceUsage)} over this
-   * method, if the balancing goal can be described in that way.
+   * <p>It is recommended to use {@link ResourceUsageHint#idealness(ResourceUsage)} over this method
+   * if possible.
    *
    * @return a {@link Predicate} that can use to determine if the resource usage of a cluster is
    *     valid.
