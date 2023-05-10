60f4d3e998ed9e73d5d5ce1233348ad0ac2ff869
Chia-Ping Tsai <chia7712@gmail.com>
2023-05-11 00:10:28 +0800

    [COMMON] rewrite admin.Offset by java 17 record

--- common/src/main/java/org/astraea/common/admin/Offset.java
+++ common/src/main/java/org/astraea/common/admin/Offset.java
@@ -16,25 +16,4 @@
  */
 package org.astraea.common.admin;
 
-public final class Offset {
-  private final long earliest;
-  private final long latest;
-
-  Offset(long earliest, long latest) {
-    this.earliest = earliest;
-    this.latest = latest;
-  }
-
-  @Override
-  public String toString() {
-    return "Offset{" + "earliest=" + earliest + ", latest=" + latest + '}';
-  }
-
-  public long earliest() {
-    return earliest;
-  }
-
-  public long latest() {
-    return latest;
-  }
-}
+public record Offset(long earliest, long latest) {}
