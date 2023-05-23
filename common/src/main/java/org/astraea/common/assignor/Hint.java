9fb2b119638dc85393489cd9f0939b42441603be
Zhi-Mao Teng <zhimao.teng@gmail.com>
2023-05-23 16:58:26 +0800

    Add GeneratorTest and modify Hint

--- common/src/main/java/org/astraea/common/assignor/Hint.java
+++ common/src/main/java/org/astraea/common/assignor/Hint.java
@@ -37,16 +37,23 @@ public interface Hint {
   static Hint lowCostHint(
       Map<String, SubscriptionInfo> subscriptions, Map<TopicPartition, Double> partitionCost) {
     return (currentAssignment, tp) -> {
-      var candidates =
+      var consumerPerCost =
           currentAssignment.entrySet().stream()
               .filter(e -> subscriptions.get(e.getKey()).topics().contains(tp.topic()))
               .map(
                   e ->
                       Map.entry(
                           e.getKey(), e.getValue().stream().mapToDouble(partitionCost::get).sum()))
-              .sorted(Map.Entry.comparingByValue())
-              .map(Map.Entry::getKey)
-              .toList();
+              .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+      List<String> candidates;
+
+      if (consumerPerCost.containsValue(0.0))
+        candidates =
+            consumerPerCost.entrySet().stream()
+                .filter(e -> e.getValue() == 0.0)
+                .map(Map.Entry::getKey)
+                .toList();
+      else candidates = consumerPerCost.entrySet().stream().map(Map.Entry::getKey).toList();
 
       return candidates.stream().limit((long) Math.ceil(candidates.size() / 2.0)).toList();
     };
