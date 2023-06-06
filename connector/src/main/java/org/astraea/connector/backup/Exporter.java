54b2973d05bfcf2ce1e680933392ee566438b6e1
Jia-Sheng Chen <haser1156@gmail.com>
2023-06-06 21:36:17 +0800

    fix format violations

--- connector/src/main/java/org/astraea/connector/backup/Exporter.java
+++ connector/src/main/java/org/astraea/connector/backup/Exporter.java
@@ -18,7 +18,6 @@ package org.astraea.connector.backup;
 
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
