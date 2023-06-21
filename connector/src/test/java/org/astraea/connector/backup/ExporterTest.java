d013aa4506ab0fc2ea4511acb2392d0d8085b6b9
Jia-Sheng Chen <haser1156@gmail.com>
2023-06-21 16:38:59 +0800

    fix the size in RecordWriter never be updated, also add a test for this.

--- connector/src/test/java/org/astraea/connector/backup/ExporterTest.java
+++ connector/src/test/java/org/astraea/connector/backup/ExporterTest.java
@@ -465,15 +465,25 @@ public class ExporterTest {
               Record.builder()
                   .topic(topicName)
                   .key("test".getBytes())
-                  .value("test0".getBytes())
+                  .value(Utils.randomString(1024).getBytes())
                   .partition(0)
+                  .offset(0)
                   .timestamp(System.currentTimeMillis())
                   .build(),
               Record.builder()
                   .topic(topicName)
                   .key("test".getBytes())
-                  .value("test1".getBytes())
+                  .value("test".getBytes())
+                  .partition(0)
+                  .offset(1)
+                  .timestamp(System.currentTimeMillis())
+                  .build(),
+              Record.builder()
+                  .topic(topicName)
+                  .key("test".getBytes())
+                  .value("test".getBytes())
                   .partition(1)
+                  .offset(0)
                   .timestamp(System.currentTimeMillis())
                   .build());
 
@@ -489,6 +499,10 @@ public class ExporterTest {
 
       Assertions.assertEquals(
           2, fs.listFolders("/" + String.join("/", fileSize, topicName)).size());
+      Assertions.assertEquals(
+          2, fs.listFiles("/" + String.join("/", fileSize, topicName, "0")).size());
+      Assertions.assertEquals(
+          1, fs.listFiles("/" + String.join("/", fileSize, topicName, "1")).size());
 
       records.forEach(
           sinkRecord -> {
